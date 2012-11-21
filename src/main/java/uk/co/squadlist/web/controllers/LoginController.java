0b9acaf56d0566c910e99a791cb55ff1768ea127
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-11-21 23:04:00 +0000

    Styling login page.

--- src/main/java/uk/co/squadlist/web/controllers/LoginController.java
+++ src/main/java/uk/co/squadlist/web/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package uk.co.squadlist.web.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +9,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.co.squadlist.web.api.SquadlistApi;
-import uk.co.squadlist.web.auth.LoggedInUserService;
-import uk.co.squadlist.web.urls.UrlBuilder;
+import uk.co.squadlist.web.model.Instance;
 
 @Controller
 public class LoginController {
 	
+	private SquadlistApi api;
+	
+	@Autowired
+	public LoginController(SquadlistApi api) {
+		this.api = api;
+	}
+
 	@RequestMapping(value="/login", method=RequestMethod.GET)
     public ModelAndView member() throws Exception {
-    	final ModelAndView mv = new ModelAndView("login");		
+    	final ModelAndView mv = new ModelAndView("login");
+    	mv.addObject("instance", getInstance());
     	return mv;
     }
+
+	private Instance getInstance() {
+		List<Instance> instances = api.getInstances();
+		for (Instance instance : instances) {
+			if (instance.getId().equals(SquadlistApi.INSTANCE)) {
+				return instance;
+			}
+		}
+		throw new RuntimeException("Unknown instance");
+	}
 	
 }
