bd0cd40083201dcd5354b457e37c1c13edf0b164
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-11-23 23:05:57 +0000

    Outings nav is correct for my outings.

--- src/main/java/uk/co/squadlist/web/controllers/MyOutingsController.java
+++ src/main/java/uk/co/squadlist/web/controllers/MyOutingsController.java
@@ -1,8 +1,13 @@
 package uk.co.squadlist.web.controllers;
 
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.co.squadlist.web.api.SquadlistApi;
@@ -25,15 +30,22 @@ public class MyOutingsController {
 	}
 	
 	@RequestMapping("/")
-    public ModelAndView outings() throws Exception {
+    public ModelAndView outings(@RequestParam(value = "month", required = false) String month) throws Exception {
     	ModelAndView mv = new ModelAndView("myOutings");
     	final String loggedInUser = loggedInUserService.getLoggedInUser();
 		mv.addObject("loggedInUser", loggedInUser);
     	
+		Date startDate = DateHelper.startOfCurrentOutingPeriod().toDate();
+		Date endDate = DateHelper.endOfCurrentOutingPeriod().toDate();		
+		if (month != null) {
+    		final DateTime monthDateTime = ISODateTimeFormat.yearMonth().parseDateTime(month);	// TODO Can be moved to spring?
+    		startDate = monthDateTime.toDate();
+    		endDate = monthDateTime.plusMonths(1).toDate();
+    	}
+		
 		mv.addObject("outings", displayObjectFactory.makeDisplayObjectsFor(api
 				.getAvailabilityFor(SquadlistApi.INSTANCE, loggedInUser,
-						DateHelper.startOfCurrentOutingPeriod().toDate(), 
-						DateHelper.endOfCurrentOutingPeriod().toDate())));
+						startDate, endDate)));
 
 		mv.addObject("outingMonths", api.getMemberOutingMonths(SquadlistApi.INSTANCE, loggedInUser));
     	mv.addObject("availabilityOptions", api.getAvailabilityOptions(SquadlistApi.INSTANCE));
