21f087e266088661367f9a3f90256ab80e4df115
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-11-23 08:53:01 +0000

    Login screen correctly shows incorrect login alert.

--- src/main/java/uk/co/squadlist/web/controllers/LoginController.java
+++ src/main/java/uk/co/squadlist/web/controllers/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.co.squadlist.web.api.SquadlistApi;
@@ -21,13 +22,16 @@ public class LoginController {
 		this.api = api;
 	}
 
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-    public ModelAndView member() throws Exception {
-    	final ModelAndView mv = new ModelAndView("login");
-    	mv.addObject("instance", getInstance());
-    	return mv;
-    }
-
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView member(@RequestParam(value = "errors", required = false) Boolean errors) throws Exception {
+		final ModelAndView mv = new ModelAndView("login");
+		mv.addObject("instance", getInstance());
+		if (errors != null && errors) {
+			mv.addObject("errors", true);
+		}
+		return mv;
+	}
+	
 	private Instance getInstance() {
 		List<Instance> instances = api.getInstances();
 		for (Instance instance : instances) {
