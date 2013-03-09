6d70329a685da06e4fe5a726cb5a25e7e24ecf7c
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-03-09 11:29:49 +0000

    Assert that member registration and points fields can be set correctly.

--- src/test/java/uk/co/squadlist/web/api/SquadlistApiIT.java
+++ src/test/java/uk/co/squadlist/web/api/SquadlistApiIT.java
@@ -16,9 +16,9 @@ import org.joda.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
 
+import uk.co.squadlist.web.exceptions.InvalidInstanceException;
 import uk.co.squadlist.web.exceptions.InvalidSquadException;
 import uk.co.squadlist.web.model.AvailabilityOption;
-import uk.co.squadlist.web.model.Instance;
 import uk.co.squadlist.web.model.Member;
 import uk.co.squadlist.web.model.Outing;
 import uk.co.squadlist.web.model.OutingAvailability;
@@ -30,74 +30,69 @@ public class SquadlistApiIT {
 	private static final String TEST_INSTANCE_PREFIX = "testit";
 
 	private SquadlistApi api;
-	private String instanceName;
+	private String instanceId;
 
 	@Before
-	public void setup() {
+	public void setup() throws Exception {
 		api = new SquadlistApi("http://localhost:9090");
-		instanceName = TEST_INSTANCE_PREFIX + UUID.randomUUID().toString();
-		
-		final List<Instance> instances = api.getInstances();
-		System.out.println(instances);
-		// TODO assert not already present
-		
-		final Instance instance = api.createInstance(instanceName, "Test instance");
-		System.out.println(instance);	
+		instanceId = TEST_INSTANCE_PREFIX + UUID.randomUUID().toString();
+		System.out.println(instanceId);
+		api.createInstance(instanceId, "Test instance");
 	}
 	
-	@Test(expected=InstanceExistException.class)
+	@Test(expected=InvalidInstanceException.class)
 	public void cannotCreateTwoInstancesWithTheSameId() throws Exception {
-		api.createInstance(instanceName, "Test instance");
+		api.createInstance(instanceId, "Test instance");
 	}
 		
 	@Test(expected=InvalidSquadException.class)
 	public void shouldValidateNewSquadRequests() throws Exception {
-		api.createSquad(instanceName, "");
+		api.createSquad(instanceId, "");
 	}
 	
 	@Test(expected=InvalidSquadException.class)
 	public void shouldNowAllowDuplicateSquadNamesWhenAddingNewSquads() throws Exception {
-		api.createSquad(instanceName, "New squad");
-		api.createSquad(instanceName, "New squad");
+		api.createSquad(instanceId, "New squad");
+		api.createSquad(instanceId, "New squad");
 	}
 	
 	@Test
 	public void canCreateAndPopulateNewInstances() throws Exception {		
 		
-		List<AvailabilityOption> availabilityOptions = api.getAvailabilityOptions(instanceName);
+		List<AvailabilityOption> availabilityOptions = api.getAvailabilityOptions(instanceId);
 		System.out.println(availabilityOptions);
 		assertTrue(availabilityOptions.isEmpty());
 		
-		final AvailabilityOption available = api.createAvailabilityOption(instanceName, "Available");
-		availabilityOptions = api.getAvailabilityOptions(instanceName);
+		final AvailabilityOption available = api.createAvailabilityOption(instanceId, "Available");
+		availabilityOptions = api.getAvailabilityOptions(instanceId);
 		System.out.println(availabilityOptions);
 		assertEquals(1, availabilityOptions.size());
 		assertEquals("Available", availabilityOptions.get(0).getLabel());
 		
-		api.createAvailabilityOption(instanceName, "Not available");
+		api.createAvailabilityOption(instanceId, "Not available");
 		
-		List<Squad> squads = api.getSquads(instanceName);
+		List<Squad> squads = api.getSquads(instanceId);
 		System.out.println(squads);
 		assertTrue(squads.isEmpty());
 		
-		Squad squad = api.createSquad(instanceName, "Novice men");
+		Squad squad = api.createSquad(instanceId, "Novice men");
 		System.out.println(squad);
 		assertEquals("novice-men", squad.getId());
 		assertEquals("Novice men", squad.getName());
 		
-		final Squad seniorWoman = api.createSquad(instanceName, "Senior women");
+		final Squad seniorWoman = api.createSquad(instanceId, "Senior women");
 		
-		squads = api.getSquads(instanceName);
+		squads = api.getSquads(instanceId);
 		assertEquals(2, squads.size());
-		List<Member> members = api.getMembers(instanceName);
+		List<Member> members = api.getMembers(instanceId);
 		System.out.println(members);
 		assertTrue(members.isEmpty());
 		
-		squad = api.getSquad(instanceName, squad.getId());
+		squad = api.getSquad(instanceId, squad.getId());
 		assertNotNull(squad);
 		
-		api.createMember(instanceName, "John", "Smith", squad);
-		members = api.getMembers(instanceName);
+		api.createMember(instanceId, "John", "Smith", squad);
+		members = api.getMembers(instanceId);
 		System.out.println(members);
 		assertEquals(1, members.size());
 		
@@ -106,60 +101,69 @@ public class SquadlistApiIT {
 		assertEquals("John", newMember.getFirstName());
 		assertEquals("Smith", newMember.getLastName());
 		
-		newMember = api.getMemberDetails(instanceName, newMember.getId());
+		newMember = api.getMemberDetails(instanceId, newMember.getId());
 		assertNotNull(newMember);
 		
 		assertFalse(newMember.getSquads().isEmpty());
 		assertEquals(newMember.getSquads().get(0).getName(), "Novice men");
 		
-		api.createMember(instanceName, "Andy", "Green", squad);
-		api.createMember(instanceName, "Tim", "Brown", squad);
-		api.createMember(instanceName, "Jane", "Smith", seniorWoman);
+		api.createMember(instanceId, "Andy", "Green", squad);
+		api.createMember(instanceId, "Tim", "Brown", squad);
+		api.createMember(instanceId, "Jane", "Smith", seniorWoman);
 		
-		List<Outing> outings = api.getOutings(instanceName);
+		List<Outing> outings = api.getOutings(instanceId);
 		System.out.println(outings);
 		assertTrue(outings.isEmpty());
 		
 		final LocalDateTime outingDate = new LocalDateTime(2012, 11, 5, 8, 0);
-		final Outing newOuting = api.createOuting(instanceName, squad.getId(), outingDate);	
+		final Outing newOuting = api.createOuting(instanceId, squad.getId(), outingDate);	
 		assertEquals("novice-men-2012-11-05-0800", newOuting.getId());
 		assertEquals("Novice men", newOuting.getSquad().getName());
 		assertEquals(outingDate.toDateTime(DateTimeZone.forID("Europe/London")), new DateTime(newOuting.getDate()));
 		
 		final LocalDateTime outingDateDuringBST = new LocalDateTime(2012, 6, 5, 8, 0, 0);
-		final Outing newOutingDuringBST = api.createOuting(instanceName, squad.getId(), outingDateDuringBST);
+		final Outing newOutingDuringBST = api.createOuting(instanceId, squad.getId(), outingDateDuringBST);
 		assertEquals("novice-men-2012-06-05-0800", newOutingDuringBST.getId());
 		assertEquals(outingDateDuringBST.toDateTime(DateTimeZone.forID("Europe/London")), new DateTime(newOutingDuringBST.getDate()));
 		assertEquals("2012-06-05T08:00:00.000+01:00", new DateTime(newOutingDuringBST.getDate()).toString());
 		
-		outings = api.getOutings(instanceName);
+		outings = api.getOutings(instanceId);
 		assertEquals(2, outings.size());
 		
-		final List<Outing> squadOutings = api.getSquadOutings(instanceName, squad.getId(), new DateTime(1970, 1, 1, 0, 0).toDate(), new DateTime(2020, 1, 1, 0, 0).toDate());
+		final List<Outing> squadOutings = api.getSquadOutings(instanceId, squad.getId(), new DateTime(1970, 1, 1, 0, 0).toDate(), new DateTime(2020, 1, 1, 0, 0).toDate());
 		assertEquals(2, squadOutings.size());
 		
-		final OutingAvailability outingAvailability = api.setOutingAvailability(instanceName, newMember.getId(), newOuting.getId(), available.getLabel());
+		final OutingAvailability outingAvailability = api.setOutingAvailability(instanceId, newMember.getId(), newOuting.getId(), available.getLabel());
 		assertEquals("Available", outingAvailability.getAvailability());
 		
-		Map<String, String> updatedOutingAvailability = api.getOutingAvailability(instanceName, newOuting.getId());
+		Map<String, String> updatedOutingAvailability = api.getOutingAvailability(instanceId, newOuting.getId());
 		assertEquals("Available", updatedOutingAvailability.get(newMember.getId()));
 		
 		final Date dayBeforeEarliestOuting = new DateTime(newOuting.getDate()).minusDays(1).toDate();
-		List<OutingAvailability> membersAvailability = api.getAvailabilityFor(instanceName, newMember.getId(), dayBeforeEarliestOuting, DateTime.now().toDate());		
+		List<OutingAvailability> membersAvailability = api.getAvailabilityFor(instanceId, newMember.getId(), dayBeforeEarliestOuting, DateTime.now().toDate());		
 		final OutingAvailability membersOutingAvailability = membersAvailability.get(0);
 		assertEquals(newOuting.getId(), membersOutingAvailability.getOuting().getId());
 		assertEquals("Available", membersOutingAvailability.getAvailability());
 		
-		final List<OutingWithSquadAvailability> squadAvailability = api.getSquadAvailability(instanceName, newOuting.getSquad().getId(), dayBeforeEarliestOuting, DateTime.now().toDate());		
+		final List<OutingWithSquadAvailability> squadAvailability = api.getSquadAvailability(instanceId, newOuting.getSquad().getId(), dayBeforeEarliestOuting, DateTime.now().toDate());		
 		final OutingWithSquadAvailability membersAvailabiltyForSquadOuting = squadAvailability.get(0);
 		assertEquals(newOuting.getId(), membersAvailabiltyForSquadOuting.getOuting().getId());
 		assertEquals("Available", membersAvailabiltyForSquadOuting.getAvailability().get(newMember.getId()));
 				
 		newMember.setFirstName("Jim");
-		api.updateMemberDetails(instanceName, newMember);
+		newMember.setRegistrationNumber("ARA-123");
+		newMember.setRowingPoints("N");
+		newMember.setScullingPoints("2");
 		
-		newMember = api.getMemberDetails(instanceName, newMember.getId());
+		api.updateMemberDetails(instanceId, newMember);
+		
+		newMember = api.getMemberDetails(instanceId, newMember.getId());
 		assertEquals("Jim", newMember.getFirstName());
+		assertEquals("ARA-123", newMember.getRegistrationNumber());
+		assertEquals("N", newMember.getRowingPoints());
+		assertEquals("2", newMember.getScullingPoints());
+
+
 	}
 	
 }
