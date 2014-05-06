ae33c4b2ebbe10a1a29eccd84b9a6d0a01d74128
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-05-06 13:59:45 +0100

    Clean up.

--- src/main/java/uk/co/squadlist/web/controllers/ContactsController.java
+++ src/main/java/uk/co/squadlist/web/controllers/ContactsController.java
@@ -2,36 +2,30 @@ package uk.co.squadlist.web.controllers;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.google.common.base.Strings;
-
 import uk.co.squadlist.web.api.SquadlistApi;
 import uk.co.squadlist.web.auth.LoggedInUserService;
 import uk.co.squadlist.web.model.Member;
 import uk.co.squadlist.web.model.Squad;
-import uk.co.squadlist.web.urls.UrlBuilder;
+
+import com.google.common.base.Strings;
 
 @Controller
 public class ContactsController {
-	
-	private static Logger log = Logger.getLogger(ContactsController.class);
-	
+		
 	private SquadlistApi api;
 	private LoggedInUserService loggedInUserService;
-	private UrlBuilder urlBuilder;
 	private InstanceConfig instanceConfig;
 	
 	@Autowired
-	public ContactsController(SquadlistApi api, LoggedInUserService loggedInUserService, UrlBuilder urlBuilder, InstanceConfig instanceConfig) {
+	public ContactsController(SquadlistApi api, LoggedInUserService loggedInUserService, InstanceConfig instanceConfig) {
 		this.api = api;
 		this.loggedInUserService = loggedInUserService;
-		this.urlBuilder = urlBuilder;
 		this.instanceConfig = instanceConfig;
 	}
 	
@@ -40,17 +34,17 @@ public class ContactsController {
 		
     	final ModelAndView mv = new ModelAndView("squadContacts");
     	mv.addObject("loggedInUser", loggedInUserService.getLoggedInUser());
-    	List<Squad> squads = api.getSquads(instanceConfig.getInstance());
-		mv.addObject("squads", squads);
-    	if (!squads.isEmpty()) {
-    		final Squad squad = resolveSquad(squadId);
-    		mv.addObject("squad", squad);
-    		mv.addObject("members", api.getSquadMembers(instanceConfig.getInstance(), squad.getId()));    		
+    	final List<Squad> allSquads = api.getSquads(instanceConfig.getInstance());
+		mv.addObject("squads", allSquads);
+    	if (!allSquads.isEmpty()) {
+    		final Squad squadToShow = resolveSquad(squadId, allSquads);
+    		mv.addObject("squad", squadToShow);
+    		mv.addObject("members", api.getSquadMembers(instanceConfig.getInstance(), squadToShow.getId()));    		
     	}
     	return mv;
     }
 
-	private Squad resolveSquad(String squadId) {
+	private Squad resolveSquad(String squadId, List<Squad> allSquads) {
     	if(!Strings.isNullOrEmpty(squadId)) {
     		return api.getSquad(instanceConfig.getInstance(), squadId);
     	}
@@ -60,11 +54,9 @@ public class ContactsController {
     		return loggedInMember.getSquads().iterator().next();
     	}
     	
-    	final List<Squad> squads = api.getSquads(instanceConfig.getInstance());
-    	if (!squads.isEmpty()) {
-    		return squads.iterator().next();
+    	if (!allSquads.isEmpty()) {
+    		return allSquads.iterator().next();
     	}
-    	
     	return null;
 	}
 	
