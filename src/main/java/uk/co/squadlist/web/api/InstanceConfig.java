9c160e4243b615c117611e81cc9f1da69fb44493
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-05-22 20:28:03 +0100

    Log request host ahead of using it to make the multi tenant instance choice.

--- src/main/java/uk/co/squadlist/web/api/InstanceConfig.java
+++ src/main/java/uk/co/squadlist/web/api/InstanceConfig.java
@@ -1,17 +1,29 @@
 package uk.co.squadlist.web.api;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 class InstanceConfig {
 	
-	@Value("#{squadlist['instance']}")
-	private String instance;
+	private final static Logger log = Logger.getLogger(InstanceConfig.class);
 	
-	public String getInstance() {
-		return instance;
+	private final String instance;
+	private final HttpServletRequest request;
+	
+	@Autowired
+	public InstanceConfig(@Value("#{squadlist['instance']}") String instance, HttpServletRequest request) {
+		this.instance = instance;
+		this.request = request;
 	}
 	
+	public String getInstance() {
+		log.info("Request host is: " + request.getServerName());
+		return instance;
+	}
 
 }
