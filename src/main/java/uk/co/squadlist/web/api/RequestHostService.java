d3330d2965a0541127cd4a848f920e2fb91f00a8
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-05-23 23:12:19 +0100

    Correct header to pull ghost from when behind mod_proxy.

--- src/main/java/uk/co/squadlist/web/api/RequestHostService.java
+++ src/main/java/uk/co/squadlist/web/api/RequestHostService.java
@@ -13,20 +13,26 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 public class RequestHostService {
 	
 	private final static Logger log = Logger.getLogger(RequestHostService.class);
+
+	private static final String X_FORWARDED_HOST = "x-forwarded-host";
 	
 	public String getRequestHost() {
 		final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
 				.currentRequestAttributes()).getRequest();
 		
+		//logVisibleHeaders(request);
+		
+		final String serverName = request.getHeader(X_FORWARDED_HOST);
+		log.info("Request host is: " + serverName);
+		return serverName;
+	}
+
+	private void logVisibleHeaders(final HttpServletRequest request) {
 		Enumeration headerNames = request.getHeaderNames();
 		while(headerNames.hasMoreElements()) {
 			String header = (String) headerNames.nextElement();
-			log.info(header + ": " + request.getHeader(header));
+			log.debug(header + ": " + request.getHeader(header));
 		}
-		
-		final String serverName = request.getHeader("host");
-		log.info("Request host is: " + serverName);
-		return serverName;
 	}
 
 }
