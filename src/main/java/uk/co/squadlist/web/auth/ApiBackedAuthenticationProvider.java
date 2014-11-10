a3b3bb286afb30f8b2cacc167162a05e94a792cc
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-11-10 18:35:32 +0000

    Pre authed user for demo sites.

--- src/main/java/uk/co/squadlist/web/auth/ApiBackedAuthenticationProvider.java
+++ src/main/java/uk/co/squadlist/web/auth/ApiBackedAuthenticationProvider.java
@@ -2,6 +2,8 @@ package uk.co.squadlist.web.auth;
 
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import uk.co.squadlist.web.api.InstanceSpecificApiClient;
+import uk.co.squadlist.web.exceptions.UnknownMemberException;
 import uk.co.squadlist.web.model.Member;
 
 import com.google.common.collect.Lists;
@@ -25,10 +28,13 @@ public class ApiBackedAuthenticationProvider extends AbstractUserDetailsAuthenti
     private static final String INVALID_USERNAME_OR_PASSWORD = "Invalid username or password";
     
     private final InstanceSpecificApiClient api;
+
+	private final HttpServletRequest request;
     
     @Autowired
-    public ApiBackedAuthenticationProvider(InstanceSpecificApiClient api) {
+    public ApiBackedAuthenticationProvider(InstanceSpecificApiClient api, HttpServletRequest request) {
 		this.api = api;
+		this.request = request;
 	}
 
 	@Override
@@ -39,6 +45,23 @@ public class ApiBackedAuthenticationProvider extends AbstractUserDetailsAuthenti
     protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authToken) throws AuthenticationException { 
     	final String password = authToken.getCredentials().toString();
     	log.info("Attempting to auth user: " + username);
+    	
+    	String preauthedUser = request.getHeader("squadlist-user");
+		if (preauthedUser != null) {
+			log.info("Pre authing user: " + preauthedUser);
+			try {
+				final Member preAuthenticatedMember = api.getMemberDetails(preauthedUser);
+				
+				Collection<SimpleGrantedAuthority> authorities = Lists.newArrayList();
+				authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+				return new org.springframework.security.core.userdetails.User(preAuthenticatedMember.getId(), null, authorities);
+				
+			} catch (UnknownMemberException e) {
+				log.error(e);
+				throw new RuntimeException(e);
+			}			
+    	}   	
+    	
 		final Member authenticatedMember = api.auth(username, password);
 		if (authenticatedMember != null) {
 			log.info("Auth successful for user: " + username);
