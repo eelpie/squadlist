1ebf54faa2c95c84fada77da29059096116d665d
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-11-19 10:01:46 +0000

    Add governing body status and age grades to CSV output.

--- src/main/java/uk/co/squadlist/web/controllers/EntryDetailsModelPopulator.java
+++ src/main/java/uk/co/squadlist/web/controllers/EntryDetailsModelPopulator.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import uk.co.eelpieconsulting.common.dates.DateFormatter;
 import uk.co.squadlist.web.annotations.RequiresSquadPermission;
 import uk.co.squadlist.web.api.InstanceSpecificApiClient;
+import uk.co.squadlist.web.localisation.GoverningBody;
 import uk.co.squadlist.web.model.Member;
 import uk.co.squadlist.web.model.Squad;
 import uk.co.squadlist.web.services.Permission;
@@ -21,14 +22,16 @@ public class EntryDetailsModelPopulator {
 	
 	private InstanceSpecificApiClient api;
 	private DateFormatter dateFormatter;
+	private GoverningBody governingBody;
 	
 	public EntryDetailsModelPopulator() {
 	}
 	
 	@Autowired
-	public EntryDetailsModelPopulator(InstanceSpecificApiClient api, DateFormatter dateFormatter) {		
+	public EntryDetailsModelPopulator(InstanceSpecificApiClient api, DateFormatter dateFormatter, GoverningBody governingBody) {		
 		this.api = api;
 		this.dateFormatter = dateFormatter;
+		this.governingBody = governingBody;
 	}
 	
 	@RequiresSquadPermission(permission=Permission.VIEW_SQUAD_ENTRY_DETAILS)
@@ -45,12 +48,20 @@ public class EntryDetailsModelPopulator {
 
 	public List<List<String>> getEntryDetailsRows(List<Member> members) {	// TOOD permissions
 		final List<List<String>> rows = Lists.newArrayList();
-		for (Member member : members) {			
-    		rows.add(Arrays.asList(new String[] {member.getFirstName(), member.getLastName(), 
-    				member.getDateOfBirth() != null ? dateFormatter.dayMonthYear(member.getDateOfBirth()) : "", 
+		for (Member member : members) {	
+			
+    		final Integer effectiveAge = member.getDateOfBirth() != null ? governingBody.getEffectiveAge(member.getDateOfBirth()) : null;
+    		final String ageGrade = effectiveAge != null ? governingBody.getAgeGrade(effectiveAge) : null;
+    		
+			rows.add(Arrays.asList(new String[] {member.getFirstName(), member.getLastName(), 
+    				member.getDateOfBirth() != null ? dateFormatter.dayMonthYear(member.getDateOfBirth()) : "",
+    				effectiveAge != null ? effectiveAge.toString() : "",
+    				ageGrade != null ? ageGrade : "",
     				member.getWeight() != null ? member.getWeight().toString() : "",
     				member.getRowingPoints(),
+    				governingBody.getRowingStatus(member.getRowingPoints()),
     				member.getScullingPoints(), 
+    				governingBody.getScullingStatus(member.getScullingPoints()),
     				member.getRegistrationNumber()}));
 		}
 		return rows;
