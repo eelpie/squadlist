dcf371228d0725b13dbcc16a0aff5a54a2f008e6
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-12-25 13:48:11 +0000

    Remove potential out of memory vector by using a finite sized in flight Facebook state collection.

--- src/main/java/uk/co/squadlist/web/controllers/social/FacebookOauthStateService.java
+++ src/main/java/uk/co/squadlist/web/controllers/social/FacebookOauthStateService.java
@@ -1,33 +1,35 @@
 package uk.co.squadlist.web.controllers.social;
 
-import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Component;
 
-import com.google.common.collect.Sets;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 @Component
 public class FacebookOauthStateService {
 	
-	private final Set<String> states;	// TODO memory overflow vector
+	private final Cache<String, String> states;
 
-	public FacebookOauthStateService() {
-		this.states = Sets.newConcurrentHashSet();
+	public FacebookOauthStateService() {		
+		this.states = CacheBuilder.newBuilder().maximumSize(10000).expireAfterAccess(1, TimeUnit.HOURS).build();
+		
 	}
 	
 	public String registerState() {
 		final String state = UUID.randomUUID().toString().replaceAll("-", "");
-		states.add(state);
+		states.put(state, state);
 		return state;
 	}
 	
 	public boolean isValid(String state) {
-		return states.contains(state);
+		return states.getIfPresent(state) != null;
 	}
 	
 	public void clearState(String state) {
-		states.remove(state);
+		states.invalidate(state);
 	}
 	
 }
