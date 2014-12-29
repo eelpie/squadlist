53ee828c0ab3954c96c0e6909aab77ced0137de1
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-12-29 13:39:17 +0000

    Test coverage.

--- src/test/java/uk/co/squadlist/web/services/PermissionsServiceTest.java
+++ src/test/java/uk/co/squadlist/web/services/PermissionsServiceTest.java
@@ -14,26 +14,43 @@ import uk.co.squadlist.web.model.Member;
 public class PermissionsServiceTest {
 
 	private static final String ADMIN_ID = "admin-id";
+	private static final String COACH_ID = "coach-id";
 	
 	@Mock
 	private InstanceSpecificApiClient api;
+
 	private Member admin;
+	private Member coach;
+
+	private PermissionsService permissionsService;
 	
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		
 		this.admin = new Member();
 		admin.setId(ADMIN_ID);
 		admin.setAdmin(true);
+		
+		this.coach = new Member();
+		coach.setId(COACH_ID);
+		coach.setAdmin(false);
+		coach.setRole("Coach");
+		
+		this.permissionsService = new PermissionsService(api);
 	}
 
 	@Test
 	public void adminsCanEditMemberDetails() throws Exception {
 		when(api.getMemberDetails(ADMIN_ID)).thenReturn(admin);
 		
-		final PermissionsService permissionsService = new PermissionsService(api);
-		
 		assertTrue(permissionsService.hasMemberPermission(ADMIN_ID, Permission.EDIT_MEMBER_DETAILS, "auser"));
+	}
+	@Test
+	public void coachesCanEditMemberDetails() throws Exception {
+		when(api.getMemberDetails(COACH_ID)).thenReturn(coach);
+		
+		assertTrue(permissionsService.hasMemberPermission(COACH_ID, Permission.EDIT_MEMBER_DETAILS, "auser"));
 	}
 	
 }
