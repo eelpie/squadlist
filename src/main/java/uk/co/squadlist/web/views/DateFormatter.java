4183c74ba4a000aed8a1c2c4caad48070ea6c18c
tony <tony@eelpieconsulting.co.uk>
2015-01-01 16:15:54 +0000

    Marking Timezone TODO.

--- src/main/java/uk/co/squadlist/web/views/DateFormatter.java
+++ src/main/java/uk/co/squadlist/web/views/DateFormatter.java
@@ -6,6 +6,7 @@ import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.stereotype.Component;
 
+// TODO needs to be aware of instance timezone
 @Component
 public class DateFormatter extends uk.co.eelpieconsulting.common.dates.DateFormatter {
 
@@ -13,9 +14,9 @@ public class DateFormatter extends uk.co.eelpieconsulting.common.dates.DateForma
 		final DateTime monthDateTime = ISODateTimeFormat.yearMonth().parseDateTime(month);
 		return monthDateTime.toString("MMM yyyy");
 	}
-	
+
 	public static String dayMonthTime(Date date) {
 		return new DateTime(date).toString("EEE dd MMM kk:mm");
 	}
-	
+
 }
