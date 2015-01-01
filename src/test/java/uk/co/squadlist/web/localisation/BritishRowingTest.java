c8cb854f8350fc1c569774e0a71768f1dff12a0e
tony <tony@eelpieconsulting.co.uk>
2015-01-01 15:22:58 +0000

    Update asserts for new year.
    Test appears correct.

--- src/test/java/uk/co/squadlist/web/localisation/BritishRowingTest.java
+++ src/test/java/uk/co/squadlist/web/localisation/BritishRowingTest.java
@@ -10,31 +10,31 @@ import org.junit.Test;
 import com.google.common.collect.Lists;
 
 public class BritishRowingTest {
-	
+
 	private final BritishRowing britishRowing = new BritishRowing();
-	
+
 	@Test
-	public void canRecogniseValidBRRegistrationNumbers() throws Exception {		
-		assertNull(britishRowing.checkRegistrationNumber("201505G1020791"));	
+	public void canRecogniseValidBRRegistrationNumbers() throws Exception {
+		assertNull(britishRowing.checkRegistrationNumber("201505G1020791"));
 	}
-	
+
 	@Test
 	public void shouldReturnValidationMessageForInvalidLookingRegistrationNumber() throws Exception {
 		assertEquals("Not in the expected British Rowing format", britishRowing.checkRegistrationNumber("2013343434"));
 	}
-	
+
 	@Test
 	public void shouldDetectValidButExpiredRegistrationNumbers() throws Exception {
 		assertEquals("Expired registration", britishRowing.checkRegistrationNumber("201405G1020791"));
 	}
-	
+
 	@Test
 	public void effectiveAgeForMastersIsTheAgeTheyWouldReachBy31DecOfTheCurrentYear() throws Exception {
-		assertEquals(39, britishRowing.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0).toDate()));		
-		assertEquals(39, britishRowing.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0).toDate()));		
-		assertEquals(39, britishRowing.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0).toDate()));		
+		assertEquals(40, britishRowing.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0).toDate()));
+		assertEquals(40, britishRowing.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0).toDate()));
+		assertEquals(40, britishRowing.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0).toDate()));
 	}
-	
+
 	@Test
 	public void pointsGradesAreBasedOnMaximumPointsAllowed() throws Exception {
 		assertEquals("Novice", britishRowing.getRowingStatus(Lists.newArrayList("0")));
@@ -45,14 +45,14 @@ public class BritishRowingTest {
 		assertEquals("Senior", britishRowing.getRowingStatus(Lists.newArrayList("9")));
 		assertEquals("Elite", britishRowing.getRowingStatus(Lists.newArrayList("10")));
 	}
-	
+
 	@Test
 	public void crewPointsCannotBeInferredIfSomeCrewMembersHaveNotProvidedPointsInformation() throws Exception {
 		List<String> rowingPoints = Lists.newArrayList("0", "1", null, "1");
 
-		assertNull(britishRowing.getRowingStatus(rowingPoints));		
+		assertNull(britishRowing.getRowingStatus(rowingPoints));
 	}
-	
+
 	@Test
 	public void canCalculateStatusForDifferentCrewSizes() throws Exception {
 		assertEquals("Novice", britishRowing.getRowingStatus(Lists.newArrayList("0")));
@@ -60,9 +60,9 @@ public class BritishRowingTest {
 		assertEquals("Intermediate 2", britishRowing.getRowingStatus(Lists.newArrayList("16", "0","0", "0")));
 		assertEquals("Intermediate 1", britishRowing.getRowingStatus(Lists.newArrayList("24", "0","0", "0")));
 		assertEquals("Senior", britishRowing.getRowingStatus(Lists.newArrayList("25", "0","0", "0")));
-		assertEquals("Elite", britishRowing.getRowingStatus(Lists.newArrayList("37", "0","0", "0")));				
+		assertEquals("Elite", britishRowing.getRowingStatus(Lists.newArrayList("37", "0","0", "0")));
 		assertEquals("Senior", britishRowing.getRowingStatus(Lists.newArrayList("72", "0", "0", "0", "0", "0", "0", "0")));
 		assertEquals("Elite", britishRowing.getRowingStatus(Lists.newArrayList("73", "0", "0", "0", "0", "0", "0", "0")));
 	}
-	
+
 }
