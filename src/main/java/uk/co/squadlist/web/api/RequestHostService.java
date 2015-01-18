03aea50d3faf61f7c7537f181d63bc202957b9ce
tony <tony@eelpieconsulting.co.uk>
2015-01-18 11:44:44 +0000

    Logging levels.

--- src/main/java/uk/co/squadlist/web/api/RequestHostService.java
+++ src/main/java/uk/co/squadlist/web/api/RequestHostService.java
@@ -9,18 +9,18 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 public class RequestHostService {
-	
+
 	private final static Logger log = Logger.getLogger(RequestHostService.class);
 
 	private static final String X_FORWARDED_HOST = "x-forwarded-host";
-	
+
 	public String getRequestHost() {
 		final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
 				.currentRequestAttributes()).getRequest();
-				
+
 		final String serverName = request.getHeader(X_FORWARDED_HOST);
-		log.info("Request host is: " + serverName);
+		log.debug("Request host is: " + serverName);
 		return serverName;
 	}
-	
+
 }
