7769d256e19d9c497110f543701e2398354d28b1
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-04-30 12:16:20 +1200

    Alert pill in the nav bar for members with a problematic registration number.

--- src/main/java/uk/co/squadlist/web/views/ViewFactory.java
+++ src/main/java/uk/co/squadlist/web/views/ViewFactory.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.co.squadlist.web.auth.LoggedInUserService;
+import uk.co.squadlist.web.localisation.GoverningBody;
 import uk.co.squadlist.web.model.Member;
 import uk.co.squadlist.web.services.OutingAvailabilityCountsService;
 import uk.co.squadlist.web.services.PreferedSquadService;
@@ -15,13 +16,15 @@ public class ViewFactory {
 	private final LoggedInUserService loggedInUserService;
 	private final OutingAvailabilityCountsService outingAvailabilityCountsService;
 	private final PreferedSquadService preferedSquadService;
+	private final GoverningBody governingBody;
 	
 	@Autowired
 	public ViewFactory(LoggedInUserService loggedInUserService, OutingAvailabilityCountsService outingAvailabilityCountsService,
-			PreferedSquadService preferedSquadService) {
+			PreferedSquadService preferedSquadService, GoverningBody governingBody) {
 		this.loggedInUserService = loggedInUserService;
 		this.outingAvailabilityCountsService = outingAvailabilityCountsService;
 		this.preferedSquadService = preferedSquadService;
+		this.governingBody = governingBody;
 	}
 
 	public ModelAndView getView(String templateName) {
@@ -30,9 +33,14 @@ public class ViewFactory {
 		final ModelAndView mv = new ModelAndView(templateName);
 		mv.addObject("loggedInUser", loggedInUser.getId());
 		
-    	int pendingOutingsCountFor = outingAvailabilityCountsService.getPendingOutingsCountFor(loggedInUser.getId());	// TODO should be a post handler?
+    	final int pendingOutingsCountFor = outingAvailabilityCountsService.getPendingOutingsCountFor(loggedInUser.getId());	// TODO should be a post handler?
     	if (pendingOutingsCountFor > 0) {
     		mv.addObject("pendingOutingsCount", pendingOutingsCountFor);
+    	}
+    	
+    	final int memberDetailsProblems = governingBody.checkRegistrationNumber(loggedInUser.getRegistrationNumber()) != null ? 1 : 0;
+    	if (memberDetailsProblems > 0) {
+    		mv.addObject("memberDetailsProblems", memberDetailsProblems);
     	}
     	
     	try {
