b9112624ed1b0911779559596e7e2ea3d6b5d89c
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-05-03 11:20:25 +1200

    Highlights the current date formatter problem with non Europe/London timezones.
    The current UI only works if the application is running in Europe/London timezone which matches the instance time zone.
    
    Hard coding Europe/London into the date formatter allows the UI to run correctly even if the application is running in a different default timezone.
    
    Making the date formatter aware of the current instances timezone will allow instance timezones other than Europe/London to work correctly.

--- src/main/java/uk/co/squadlist/web/views/DateFormatter.java
+++ src/main/java/uk/co/squadlist/web/views/DateFormatter.java
@@ -3,6 +3,7 @@ package uk.co.squadlist.web.views;
 import java.util.Date;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +11,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class DateFormatter extends uk.co.eelpieconsulting.common.dates.DateFormatter {
 
+	private static DateTimeZone timeZone = DateTimeZone.forID("Europe/London");
+
 	public static String month(String month) {
 		final DateTime monthDateTime = ISODateTimeFormat.yearMonth().parseDateTime(month);
 		return monthDateTime.toString("MMM yyyy");
 	}
 
 	public static String dayMonthTime(Date date) {
-		return new DateTime(date).toString("EEE dd MMM kk:mm");
+		return new DateTime(date, timeZone).toString("EEE dd MMM kk:mm");
 	}
 
 }
