be0aa77711bda1831df8684a78ff5727a0a889fc
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-05-04 15:59:06 +1200

    Optimisation - cache the context instance on the request.
    Prevents n calls to the instance API endpoint when formatting lists of dates.

--- src/main/java/uk/co/squadlist/web/context/Context.java
+++ src/main/java/uk/co/squadlist/web/context/Context.java
@@ -1,31 +1,50 @@
 package uk.co.squadlist.web.context;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import uk.co.squadlist.web.api.InstanceSpecificApiClient;
 import uk.co.squadlist.web.exceptions.UnknownInstanceException;
+import uk.co.squadlist.web.model.Instance;
 
 @Component
 public class Context {
 
+	private static final String INSTANCE = "instance";
+
 	private final static Logger log = Logger.getLogger(Context.class);
 	
-	private final InstanceSpecificApiClient api;
-	
+	private InstanceSpecificApiClient api;
+
+	private final HttpServletRequest request;
+
 	@Autowired
-	public Context(InstanceSpecificApiClient api) {
+	public Context(InstanceSpecificApiClient api, HttpServletRequest request) {
 		this.api = api;
+		this.request = request;
 	}
 
 	public String getTimeZone() {
 		try {
-			return api.getInstance().getTimeZone();
+			Instance instance = getInstance();
+			return instance.getTimeZone();
 		} catch (UnknownInstanceException e) {
 			log.error(e);
 			throw new RuntimeException(e);
 		}
+	}
+
+	private Instance getInstance() throws UnknownInstanceException {
+		Instance instance = (Instance) request.getAttribute(INSTANCE);
+		if (instance != null) {
+			return instance;
+		}
+		instance = api.getInstance();
+		request.setAttribute(INSTANCE, instance);
+		return instance;
 	}
 	
 }
