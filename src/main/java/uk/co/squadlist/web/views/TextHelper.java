fe495afccd1953b8617939e763b5e733572b6f6d
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-05-06 20:59:05 +1200

    Can select language from context.

--- src/main/java/uk/co/squadlist/web/views/TextHelper.java
+++ src/main/java/uk/co/squadlist/web/views/TextHelper.java
@@ -6,31 +6,43 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import uk.co.squadlist.web.context.Context;
 import uk.co.squadlist.web.localisation.text.PropertiesFileParser;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 @Component
 public class TextHelper {
 	
-	private Map<String, String> text;
-
+	private static final String ENGLISH = "en";
+	private static final String FRENCH = "fr";
+	
+	private final Context context;
+	
+	private Map<String, Map<String, String>> text;
+	
 	@Autowired
-	public TextHelper(PropertiesFileParser propertiesFileParser) {
-		this.text = propertiesFileParser.readTextPropertiesFromFile("EN.properties");
+	public TextHelper(PropertiesFileParser propertiesFileParser, Context context) {
+		this.context = context;
+		
+		this.text = Maps.newHashMap();
+		for (String availableLanguage : Lists.newArrayList(ENGLISH, FRENCH)) {
+			text.put(availableLanguage, propertiesFileParser.readTextPropertiesFromFile(availableLanguage + ".properties"));
+		}
 	}
 	
 	public String text(String key) {
-		if (text.containsKey(key)) {
-			return text.get(key);
-		}		
+		if (text.get(language()).containsKey(key)) {
+			return text.get(language()).get(key);
+		}
 		return key;
 	}
 	
 	public String text(String key, String... values) {
-		if (text.containsKey(key)) {
-			String value = text.get(key);
+		if (text.get(language()).containsKey(key)) {
+			String value = text.get(language()).get(key);
 			Iterator<String> split = Splitter.on("{}").split(value).iterator();			
 			Iterator<String> valueIterator = Lists.newArrayList(values).iterator();		
 			
@@ -43,6 +55,13 @@ public class TextHelper {
 			return output.toString();
 		}
 		return key;
+	}
+	
+	private String language() {
+		if ("France/Paris".equals(context.getTimeZone())) {
+			return FRENCH;
+		}
+		return ENGLISH;
 	}
 
 }
--- src/test/java/uk/co/squadlist/web/views/TextHelperTest.java
+++ src/test/java/uk/co/squadlist/web/views/TextHelperTest.java
@@ -1,15 +1,16 @@
 package uk.co.squadlist.web.views;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import uk.co.squadlist.web.context.Context;
 import uk.co.squadlist.web.localisation.text.PropertiesFileParser;
 
 import com.google.common.collect.Maps;
@@ -18,22 +19,43 @@ public class TextHelperTest {
 
 	@Mock
 	private PropertiesFileParser propertiesFileParser;
+	@Mock
+	private Context context;
+	private TextHelper textHelper;
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		
+		Map<String, String> englishKeys = Maps.newHashMap();
+		englishKeys.put("login", "Login");
+		englishKeys.put("require.access", "If you are a {} member who requires access to this copy of Squadlist please contact your coach.");
+		when(propertiesFileParser.readTextPropertiesFromFile("en.properties")).thenReturn(englishKeys);
+		
+		Map<String, String> frenchKeys = Maps.newHashMap();
+		frenchKeys.put("login", "Connexion");
+		when(propertiesFileParser.readTextPropertiesFromFile("fr.properties")).thenReturn(frenchKeys);
+		
+		this.textHelper = new TextHelper(propertiesFileParser, context);
+	}
+	
+	@Test
+	public void canResolveLocalisedTextByKey() throws Exception {
+		assertEquals("Login", textHelper.text("login"));
 	}
 		
 	@Test
 	public void canSubstitutePlaceHolderValues() throws Exception {		
-		Map<String, String> keys = Maps.newHashMap();
-		keys.put("require.access", "If you are a {} member who requires access to this copy of Squadlist please contact your coach.");
-		Mockito.when(propertiesFileParser.readTextPropertiesFromFile("EN.properties")).thenReturn(keys);		
-		TextHelper textHelper = new TextHelper(propertiesFileParser);
-
-		final String text = textHelper.text("require.access", "Twickenham");
+		assertEquals("If you are a Twickenham member who requires access to this copy of Squadlist please contact your coach.", textHelper.text("require.access", "Twickenham"));
+	}
+	
+	@Test
+	public void shouldUseFrenchTextForFrenchContexts() throws Exception {	
+		when(context.getTimeZone()).thenReturn("France/Paris");
 		
-		assertEquals("If you are a Twickenham member who requires access to this copy of Squadlist please contact your coach.", text);
+		final String text = textHelper.text("login");
+		
+		assertEquals("Connexion", text);		
 	}
 	
 }
