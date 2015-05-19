337a1a55d88e81cda7e472642d83d411054f79a5
tony <tony@eelpieconsulting.co.uk>
2015-05-19 10:08:06 +0100

    Refactor towards a configurable input string.

--- src/main/java/uk/co/squadlist/web/context/CustomInstanceUrls.java
+++ src/main/java/uk/co/squadlist/web/context/CustomInstanceUrls.java
@@ -1,19 +1,21 @@
 package uk.co.squadlist.web.context;
 
+import java.util.Iterator;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.Maps;
 
 @Component
 public class CustomInstanceUrls {
 
-	private final  Map<String, String> customInstanceUrls;
+	private final Map<String, String> customInstanceUrls;
 
 	public CustomInstanceUrls() {
-		customInstanceUrls = Maps.newHashMap();
-		customInstanceUrls.put("avail.twickenhamrc.net", "twickenham");
+		final String customUrls = "avail.twickenhamrc.net|twickenham";
+		customInstanceUrls = parseConfig(customUrls);
 	}
 
 	public String customUrl(final String requestHost) {
@@ -29,6 +31,23 @@ public class CustomInstanceUrls {
 
 	public String customUrlForInstance(String instance) {
 		return customInstanceUrls.get(instance);
+	}
+
+	private Map<String, String> parseConfig(final String customUrls) {
+		Map<String, String> customInstanceUrls = Maps.newHashMap();
+		Iterator<String> i = Splitter.on(",").split(customUrls).iterator();
+		while (i.hasNext()) {
+			String hostInstance = i.next();
+			Iterator<String> hi = Splitter.on("|").split(hostInstance).iterator();
+			if (hi.hasNext()) {
+				String host = hi.next();
+				if (hi.hasNext()) {
+					String instance = hi.next();
+					customInstanceUrls.put(host, instance);
+				}
+			}
+		}
+		return customInstanceUrls;
 	}
 
 }
