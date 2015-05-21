97a0c2b336809fe1908709ab01cb6d9cf06f9b3a
tony <tony@eelpieconsulting.co.uk>
2015-05-21 14:37:50 +0100

    Fixes missing place holders in templated emails?

--- src/main/java/uk/co/squadlist/web/services/email/EmailMessageComposer.java
+++ src/main/java/uk/co/squadlist/web/services/email/EmailMessageComposer.java
@@ -15,40 +15,44 @@ import org.springframework.ui.velocity.VelocityEngineFactory;
 import uk.co.squadlist.web.model.Instance;
 import uk.co.squadlist.web.model.Member;
 import uk.co.squadlist.web.urls.UrlBuilder;
+import uk.co.squadlist.web.views.TextHelper;
 
 @Component
 public class EmailMessageComposer {
-	
+
 	private final static Logger log = Logger.getLogger(EmailMessageComposer.class);
-	
+
 	private final UrlBuilder urlBuilder;
 	private final VelocityEngine velocityEngine;
-	
+	private final TextHelper textHelper;
+
 	@Autowired
-	public EmailMessageComposer(VelocityEngineFactory velocityEngineFactory, UrlBuilder urlBuilder) throws VelocityException, IOException {
+	public EmailMessageComposer(VelocityEngineFactory velocityEngineFactory, UrlBuilder urlBuilder, TextHelper textHelper) throws VelocityException, IOException {
 		this.urlBuilder = urlBuilder;
-		velocityEngine = velocityEngineFactory.createVelocityEngine();			
+		this.textHelper = textHelper;
+		velocityEngine = velocityEngineFactory.createVelocityEngine();
 	}
-	
+
 	public String composeNewMemberInviteMessage(Instance instance, Member member, String initialPassword) {
-		try {			
+		try {
 			Template template = velocityEngine.getTemplate("email/invite.vm");
-			
+
 			final VelocityContext context = new VelocityContext();
 			context.put("instance", instance);
 			context.put("instanceUrl", urlBuilder.getBaseUrl());
 			context.put("member", member);
 			context.put("initialPassword", initialPassword);
-			
+			context.put("text", textHelper);
+
 			final StringWriter output = new StringWriter();
 			template.merge(context, output);
-			
+
 			return output.toString();
-			
+
 		} catch (VelocityException e) {
 			log.error(e);
 			throw new RuntimeException();
 		}
 	}
-	
+
 }
