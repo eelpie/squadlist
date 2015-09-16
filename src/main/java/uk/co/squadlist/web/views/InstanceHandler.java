41f1f7fd2aa8f79c2b0fd51f2d6e2a8c58115ae1
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-09-16 20:38:09 +0100

    Correct wiring.

--- src/main/java/uk/co/squadlist/web/views/InstanceHandler.java
+++ src/main/java/uk/co/squadlist/web/views/InstanceHandler.java
@@ -3,6 +3,7 @@ package uk.co.squadlist.web.views;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
