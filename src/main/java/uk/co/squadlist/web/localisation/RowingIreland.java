94d008b6f881b2086042baf70f26666160a73d06
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-01-18 13:42:37 +0000

    RowingIreland points range from 0 to 1000 in increments of 10 for now.

--- src/main/java/uk/co/squadlist/web/localisation/RowingIreland.java
+++ src/main/java/uk/co/squadlist/web/localisation/RowingIreland.java
@@ -1,8 +1,8 @@
 package uk.co.squadlist.web.localisation;
 
+import com.google.common.base.Function;
 import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import com.google.common.collect.*;
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.Years;
@@ -15,7 +15,13 @@ import java.util.regex.Pattern;
 public class RowingIreland extends BaseGoverningBody implements GoverningBody {
 
 	private static final Pattern VALID_REGISTRATION_NUMBER = Pattern.compile("\\d{1,5}");
-	private static final List<String> POINTS_OPTIONS = Lists.newArrayList("N", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");	// TODO N is deprecated - move to 0
+
+	private static final List<String> POINTS_OPTIONS = Lists.newArrayList(Collections2.transform(ContiguousSet.create(Range.open(0, 100), DiscreteDomain.integers()).asList(), new Function<Integer, String>() {
+		@Override
+		public String apply(final Integer i) {
+			return Integer.toString(i * 10);
+		}
+	}));
 
 	private final Map<String, Integer> statusMaximumPoints;
 	private final Map<String, Integer> mastersMinimumAges;
