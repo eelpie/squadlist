a857706eca89ba846b9c2ae37e0df1dee0e4d304
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-11-02 11:10:30 +0000

    Stop adding no cache headers to static assets.

--- src/main/java/uk/co/squadlist/web/views/interceptors/NoCacheHandler.java
+++ src/main/java/uk/co/squadlist/web/views/interceptors/NoCacheHandler.java
@@ -1,17 +1,27 @@
 package uk.co.squadlist.web.views.interceptors;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Component
 public class NoCacheHandler extends HandlerInterceptorAdapter {
 
+	private final static Logger log = Logger.getLogger(NoCacheHandler.class);
+
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		response.addHeader("Cache-Control", "no-cache");	
+		String path = request.getPathInfo();
+		boolean cachable = path.startsWith("/assets/");
+		if (!cachable) {
+			log.info("Path is not cachable; adding no cache headers: " + path);
+			response.addHeader("Cache-Control", "no-cache");
+		} else {
+			log.info("Path is cachable: " + path);
+		}
 	}
 
 }
