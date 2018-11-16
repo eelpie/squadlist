a66b7d84c3d572f53dc6cbc20ffae648d3c70bdf
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-16 21:36:10 +0000

    Kotlin conversion.

---
+++ src/main/kotlin/uk/co/squadlist/web/controllers/LoginController.kt
@@ -0,0 +1,64 @@
+package uk.co.squadlist.web.controllers
+
+import org.apache.log4j.Logger
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.view.RedirectView
+import uk.co.squadlist.web.api.InstanceSpecificApiClient
+import uk.co.squadlist.web.auth.LoggedInUserService
+import uk.co.squadlist.web.model.Member
+import uk.co.squadlist.web.urls.UrlBuilder
+
+@Controller
+class LoginController(val api: InstanceSpecificApiClient, val loggedInUserService: LoggedInUserService, val urlBuilder: UrlBuilder) {
+
+    private val log = Logger.getLogger(LoginController::class.java)
+
+    @RequestMapping(value = "/login", method = arrayOf(RequestMethod.GET, RequestMethod.HEAD))     // TODO SEO this onto the root url
+    fun login(): ModelAndView {
+        return renderLoginScreen(false, null)
+    }
+
+    @PostMapping(value = "/login")
+    fun loginSubmit(
+            @RequestParam(value = "username", required = true) username: String,
+            @RequestParam(value = "password", required = true) password: String): ModelAndView {
+
+        log.info("Attempting to auth user: $username")
+        auth(username, password).let { token ->
+            verify(token!!).let { user ->
+                log.info("Auth successful for user: " + user!!.username)
+                loggedInUserService.setSignedIn(token)
+                return ModelAndView(RedirectView(urlBuilder.baseUrl))
+            }
+        }
+
+        return renderLoginScreen(true, username)
+    }
+
+    @GetMapping(value = "/logout")
+    fun logout(): ModelAndView {
+        loggedInUserService.cleanSignedIn()
+        return ModelAndView(RedirectView(urlBuilder.loginUrl()))
+    }
+
+    private fun renderLoginScreen(errors: Boolean, username: String?): ModelAndView {
+        val instance = api.instance
+        return ModelAndView("login").addAllObjects(mapOf(
+                "title" to instance.name,
+                "username" to username,
+                "errors" to errors
+        ))
+    }
+
+    private fun auth(username: String, password: String): String? { // TODO inline once api is migrated
+        val auth: String? = api.auth(username, password)
+        return auth
+    }
+
+    private fun verify(token: String): Member? {
+        return api.verify(token)
+    }
+
+}
