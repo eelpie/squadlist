592cbf619db156a038a2dd734f5cdf73311734ab
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-17 19:13:14 +0000

    Port.

---
+++ src/main/kotlin/uk/co/squadlist/web/controllers/ResetPasswordController.kt
@@ -0,0 +1,66 @@
+package uk.co.squadlist.web.controllers
+
+import com.google.common.base.Strings
+import org.apache.log4j.Logger
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
+import uk.co.squadlist.web.api.InstanceSpecificApiClient
+import uk.co.squadlist.web.exceptions.UnknownMemberException
+
+@Controller
+class ResetPasswordController(val api: InstanceSpecificApiClient) {
+
+    private val log = Logger.getLogger(ResetPasswordController::class.java)
+
+    @GetMapping(value = "/reset-password")
+    fun resetPasswordPrompt(): ModelAndView {
+        val mv = ModelAndView("resetPassword")
+        mv.addObject("title", "Reset password")
+        return mv
+    }
+
+    @PostMapping(value = "/reset-password")
+    fun resetPassword(@RequestParam(value = "username", required = false) username: String): ModelAndView {
+        if (Strings.isNullOrEmpty(username)) {
+            val mv = ModelAndView("resetPassword")
+            mv.addObject("errors", true)
+            mv.addObject("title", "Reset password")
+            return mv
+        }
+
+        log.info("Reseting password for: $username")
+        try {
+            api.resetPassword(username)    // TODO errors
+            log.info("Reset password call successful for: $username")
+            val mv = ModelAndView("resetPasswordSent")
+            mv.addObject("title", "Reset password")
+            return mv
+
+        } catch (e: UnknownMemberException) {
+            val mv = ModelAndView("resetPassword")
+            mv.addObject("title", "Reset password")
+            mv.addObject("errors", true)
+            return mv
+        }
+
+    }
+
+    @GetMapping(value = "/reset-password/confirm")
+    fun confirmPasswordReset(@RequestParam token: String): ModelAndView {
+        try {
+            val newPassword = api.confirmResetPassword(token)
+
+            val mv = ModelAndView("resetPasswordConfirm")
+            mv.addObject("newPassword", newPassword)
+            return mv
+
+        } catch (e: Exception) {
+            return ModelAndView("resetPasswordInvalidToken")
+        }
+
+    }
+
+}
