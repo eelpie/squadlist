b085e352c1bef2fb512c44f5d4c3cf3bed9899f3
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-17 19:10:45 +0000

    Port.

---
+++ src/main/kotlin/uk/co/squadlist/web/controllers/ContactsController.kt
@@ -0,0 +1,39 @@
+package uk.co.squadlist.web.controllers
+
+import org.apache.log4j.Logger
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.servlet.ModelAndView
+import uk.co.squadlist.web.api.InstanceSpecificApiClient
+import uk.co.squadlist.web.services.PreferedSquadService
+import uk.co.squadlist.web.views.ViewFactory
+
+@Controller
+class ContactsController(val api: InstanceSpecificApiClient, val preferedSquadService: PreferedSquadService, val viewFactory: ViewFactory, val contactsModelPopulator: ContactsModelPopulator) {
+
+    private val log = Logger.getLogger(ContactsController::class.java)
+
+    @GetMapping("/contacts")
+    fun contacts(): ModelAndView {
+        val mv = viewFactory.getViewForLoggedInUser("contacts")
+        val allSquads = api.squads
+        mv.addObject("squads", allSquads)    // TODO leaves squad null on view
+        return mv
+    }
+
+    @GetMapping("/contacts/{squadId}")
+    fun squadContacts(@PathVariable squadId: String): ModelAndView {
+        val squadToShow = preferedSquadService.resolveSquad(squadId)
+
+        val mv = viewFactory.getViewForLoggedInUser("contacts")
+        val allSquads = api.squads
+        mv.addObject("squads", allSquads)
+        if (!allSquads.isEmpty()) {
+            log.info("Squad to show: $squadToShow")
+            contactsModelPopulator.populateModel(squadToShow, mv)
+        }
+        return mv
+    }
+
+}
