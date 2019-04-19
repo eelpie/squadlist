77894cd356bb3d510d4dde46e87c8158e4309807
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-19 11:08:02 +0100

    Time marches on; update tests.

--- src/test/java/uk/co/squadlist/web/localisation/BritishRowingTest.java
+++ src/test/java/uk/co/squadlist/web/localisation/BritishRowingTest.java
@@ -45,9 +45,9 @@ public class BritishRowingTest {
 
 	@Test
 	public void effectiveAgeForMastersIsTheAgeTheyWouldReachBy31DecOfTheCurrentYear() throws Exception {
-		assertEquals(43, britishRowing.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0).toDate()));
-		assertEquals(43, britishRowing.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0).toDate()));
-		assertEquals(43, britishRowing.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0).toDate()));
+		assertEquals(44, britishRowing.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0).toDate()));
+		assertEquals(44, britishRowing.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0).toDate()));
+		assertEquals(44, britishRowing.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0).toDate()));
 	}
 
 	@Test
