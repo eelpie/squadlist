2762ca1fddfdecae908011996a989d6887cd3075
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-06-19 18:30:56 +0100

    More insightful error page logging.

--- src/main/java/uk/co/squadlist/web/controllers/ErrorController.java
+++ src/main/java/uk/co/squadlist/web/controllers/ErrorController.java
@@ -2,33 +2,47 @@ package uk.co.squadlist.web.controllers;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController {
 
-	private final static Logger log = Logger.getLogger(ErrorController.class);
+    private final static Logger log = Logger.getLogger(ErrorController.class);
 
-	private final HttpServletRequest request;
+    private final HttpServletRequest request;
 
-	@Autowired
-	public ErrorController(HttpServletRequest request) {
-		this.request = request;
-	}
+    @Autowired
+    public ErrorController(HttpServletRequest request) {
+        this.request = request;
+    }
 
-	@RequestMapping("/error")
-	public ModelAndView error() throws Exception {
-		log.warn("Error page shown for path: " + request.getPathInfo());	// TODO how to get the original request before spring rewrites it to /error
-		return new ModelAndView("404");
-	}
+    @RequestMapping("/error")
+    public ModelAndView error() {
+        String path = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Integer statusCode = null;
+        if (status != null) {
+            statusCode = Integer.valueOf(status.toString());
+        }
 
-	@Override
-	public String getErrorPath() {
-		return "/error";
-	}
+        if (statusCode != null && statusCode == HttpStatus.NOT_FOUND.value()) {
+            log.info("404 page shown for path: " + path);
+            return new ModelAndView("404");
+        }
+
+        log.info("Error page shown for status " + statusCode + " on path: " + path);
+        return new ModelAndView("404");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 
 }
