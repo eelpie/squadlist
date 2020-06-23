8a7148bffbac29bad78de69e5cec8e428a3ec65a
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-06-23 17:51:44 +0100

    Clean up; controllers should all be using logged in user APIs.

--- src/main/java/uk/co/squadlist/web/controllers/EntryDetailsController.java
+++ src/main/java/uk/co/squadlist/web/controllers/EntryDetailsController.java
@@ -30,118 +30,117 @@ import java.util.List;
 @Controller
 public class EntryDetailsController {
 
-  private final static Logger log = Logger.getLogger(EntryDetailsController.class);
+    private final static Logger log = Logger.getLogger(EntryDetailsController.class);
 
-  private final InstanceSpecificApiClient instanceSpecificApiClient;
-  private final PreferedSquadService preferedSquadService;
-  private final ViewFactory viewFactory;
-  private final EntryDetailsModelPopulator entryDetailsModelPopulator;
-  private final CsvOutputRenderer csvOutputRenderer;
-  private final GoverningBodyFactory governingBodyFactory;
-  private final SquadlistApi squadlistApi;
-  private final LoggedInUserService loggedInUserService;
-  private final SquadlistApiFactory squadlistApiFactory;
+    private final InstanceSpecificApiClient instanceSpecificApiClient;
+    private final PreferedSquadService preferedSquadService;
+    private final ViewFactory viewFactory;
+    private final EntryDetailsModelPopulator entryDetailsModelPopulator;
+    private final CsvOutputRenderer csvOutputRenderer;
+    private final GoverningBodyFactory governingBodyFactory;
+    private final LoggedInUserService loggedInUserService;
 
-  @Autowired
-  public EntryDetailsController(InstanceSpecificApiClient instanceSpecificApiClient, PreferedSquadService preferedSquadService, ViewFactory viewFactory,
-                                EntryDetailsModelPopulator entryDetailsModelPopulator,
-                                CsvOutputRenderer csvOutputRenderer, GoverningBodyFactory governingBodyFactory,
-                                SquadlistApiFactory squadlistApiFactory, LoggedInUserService loggedInUserService) throws IOException {
-    this.instanceSpecificApiClient = instanceSpecificApiClient;
-    this.preferedSquadService = preferedSquadService;
-    this.viewFactory = viewFactory;
-    this.entryDetailsModelPopulator = entryDetailsModelPopulator;
-    this.csvOutputRenderer = csvOutputRenderer;
-    this.governingBodyFactory = governingBodyFactory;
-    this.loggedInUserService = loggedInUserService;
-    this.squadlistApiFactory = squadlistApiFactory;
-    this.squadlistApi = squadlistApiFactory.createClient();
-  }
-
-  @RequestMapping("/entrydetails/{squadId}")
-  public ModelAndView entrydetails(@PathVariable String squadId) throws Exception {
-    final ModelAndView mv = viewFactory.getViewForLoggedInUser("entryDetails");
-    mv.addObject("squads", instanceSpecificApiClient.getSquads());
-    mv.addObject("governingBody", governingBodyFactory.getGoverningBody());
-
-    final Squad squadToShow = preferedSquadService.resolveSquad(squadId);
-    entryDetailsModelPopulator.populateModel(squadToShow, mv);
-    return mv;
-  }
-
-  @RequestMapping("/entrydetails/ajax")
-  public ModelAndView ajax(@RequestBody String json) throws Exception {
-    SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
-
-    List<Member> selectedMembers = Lists.newArrayList();
-
-    JsonNode readTree = new ObjectMapper().readTree(json);
-    Iterator<JsonNode> iterator = readTree.iterator();
-    while (iterator.hasNext()) {
-      selectedMembers.add(loggedInUserApi.getMember(iterator.next().asText()));
+    @Autowired
+    public EntryDetailsController(InstanceSpecificApiClient instanceSpecificApiClient, PreferedSquadService preferedSquadService, ViewFactory viewFactory,
+                                  EntryDetailsModelPopulator entryDetailsModelPopulator,
+                                  CsvOutputRenderer csvOutputRenderer, GoverningBodyFactory governingBodyFactory,
+                                  LoggedInUserService loggedInUserService) {
+        this.instanceSpecificApiClient = instanceSpecificApiClient;
+        this.preferedSquadService = preferedSquadService;
+        this.viewFactory = viewFactory;
+        this.entryDetailsModelPopulator = entryDetailsModelPopulator;
+        this.csvOutputRenderer = csvOutputRenderer;
+        this.governingBodyFactory = governingBodyFactory;
+        this.loggedInUserService = loggedInUserService;
     }
 
-    List<String> rowingPoints = Lists.newArrayList();
-    List<String> scullingPoints = Lists.newArrayList();
-    for (Member member : selectedMembers) {
-      rowingPoints.add(member.getRowingPoints());
-      scullingPoints.add(member.getScullingPoints());
+    @RequestMapping("/entrydetails/{squadId}")
+    public ModelAndView entrydetails(@PathVariable String squadId) throws Exception {
+        final Squad squadToShow = preferedSquadService.resolveSquad(squadId);
+
+        final ModelAndView mv = viewFactory.getViewForLoggedInUser("entryDetails").
+                addObject("squads", instanceSpecificApiClient.getSquads()).
+                addObject("governingBody", governingBodyFactory.getGoverningBody());
+        entryDetailsModelPopulator.populateModel(squadToShow, mv);
+        return mv;
     }
 
-    final ModelAndView mv = viewFactory.getViewForLoggedInUser("entryDetailsAjax");
-    if (!selectedMembers.isEmpty()) {
-      mv.addObject("members", selectedMembers);
+    @RequestMapping("/entrydetails/ajax")
+    public ModelAndView ajax(@RequestBody String json) throws Exception {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
 
-      final GoverningBody governingBody = governingBodyFactory.getGoverningBody();
+        List<Member> selectedMembers = Lists.newArrayList();
 
-      int crewSize = selectedMembers.size();
-      final boolean isFullBoat = governingBody.getBoatSizes().contains(crewSize);
-      mv.addObject("ok", isFullBoat);
-      if (isFullBoat) {
-        mv.addObject("rowingPoints", governingBody.getTotalPoints(rowingPoints));
-        mv.addObject("rowingStatus", governingBody.getRowingStatus(rowingPoints));
+        JsonNode readTree = new ObjectMapper().readTree(json);
+        Iterator<JsonNode> iterator = readTree.iterator();
+        while (iterator.hasNext()) {
+            selectedMembers.add(loggedInUserApi.getMember(iterator.next().asText()));
+        }
 
-        mv.addObject("scullingPoints", governingBody.getTotalPoints(scullingPoints));
-        mv.addObject("scullingStatus", governingBody.getScullingStatus(scullingPoints));
-
-        List<Date> datesOfBirth = Lists.newArrayList();
+        List<String> rowingPoints = Lists.newArrayList();
+        List<String> scullingPoints = Lists.newArrayList();
         for (Member member : selectedMembers) {
-          datesOfBirth.add(member.getDateOfBirth());
+            rowingPoints.add(member.getRowingPoints());
+            scullingPoints.add(member.getScullingPoints());
         }
 
-        Integer effectiveAge = governingBody.getEffectiveAge(datesOfBirth);
-        if (effectiveAge != null) {
-          mv.addObject("effectiveAge", effectiveAge);
-          mv.addObject("ageGrade", governingBody.getAgeGrade(effectiveAge));
+        final ModelAndView mv = viewFactory.getViewForLoggedInUser("entryDetailsAjax");
+        if (!selectedMembers.isEmpty()) {
+            mv.addObject("members", selectedMembers);
+
+            final GoverningBody governingBody = governingBodyFactory.getGoverningBody();
+
+            int crewSize = selectedMembers.size();
+            final boolean isFullBoat = governingBody.getBoatSizes().contains(crewSize);
+            mv.addObject("ok", isFullBoat);
+            if (isFullBoat) {
+                mv.addObject("rowingPoints", governingBody.getTotalPoints(rowingPoints));
+                mv.addObject("rowingStatus", governingBody.getRowingStatus(rowingPoints));
+
+                mv.addObject("scullingPoints", governingBody.getTotalPoints(scullingPoints));
+                mv.addObject("scullingStatus", governingBody.getScullingStatus(scullingPoints));
+
+                List<Date> datesOfBirth = Lists.newArrayList();
+                for (Member member : selectedMembers) {
+                    datesOfBirth.add(member.getDateOfBirth());
+                }
+
+                Integer effectiveAge = governingBody.getEffectiveAge(datesOfBirth);
+                if (effectiveAge != null) {
+                    mv.addObject("effectiveAge", effectiveAge);
+                    mv.addObject("ageGrade", governingBody.getAgeGrade(effectiveAge));
+                }
+            }
         }
-      }
-    }
-    return mv;
-  }
-
-  @RequestMapping(value = "/entrydetails/{squadId}.csv", method = RequestMethod.GET)
-  public void entrydetailsCSV(@PathVariable String squadId, HttpServletResponse response) throws Exception {
-    viewFactory.getViewForLoggedInUser("entryDetails");  // TODO
-
-    final Squad squadToShow = preferedSquadService.resolveSquad(squadId);
-    final List<Member> squadMembers = squadlistApi.getSquadMembers(squadToShow.getId());
-
-    List<List<String>> entryDetailsRows = entryDetailsModelPopulator.getEntryDetailsRows(squadMembers);
-    csvOutputRenderer.renderCsvResponse(response, entryDetailsModelPopulator.getEntryDetailsHeaders(), entryDetailsRows);
-  }
-
-  @RequestMapping(value = "/entrydetails/selected.csv", method = RequestMethod.GET) // TODO Unused
-  public void entrydetailsSelectedCSV(@RequestParam String members, HttpServletResponse response) throws Exception {
-    log.info("Selected members: " + members);
-    List<Member> selectedMembers = Lists.newArrayList();
-    final Iterator<String> iterator = Splitter.on(",").split(members).iterator();
-    while (iterator.hasNext()) {
-      final String selectedMemberId = iterator.next();
-      log.info("Selected member id: " + selectedMemberId);
-      selectedMembers.add(squadlistApi.getMember(selectedMemberId));
+        return mv;
     }
 
-    csvOutputRenderer.renderCsvResponse(response, entryDetailsModelPopulator.getEntryDetailsHeaders(), entryDetailsModelPopulator.getEntryDetailsRows(selectedMembers));
-  }
+    @RequestMapping(value = "/entrydetails/{squadId}.csv", method = RequestMethod.GET)
+    public void entrydetailsCSV(@PathVariable String squadId, HttpServletResponse response) throws Exception {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
+
+        viewFactory.getViewForLoggedInUser("entryDetails");  // TODO
+
+        final Squad squadToShow = preferedSquadService.resolveSquad(squadId);
+        final List<Member> squadMembers = loggedInUserApi.getSquadMembers(squadToShow.getId());
+
+        List<List<String>> entryDetailsRows = entryDetailsModelPopulator.getEntryDetailsRows(squadMembers);
+        csvOutputRenderer.renderCsvResponse(response, entryDetailsModelPopulator.getEntryDetailsHeaders(), entryDetailsRows);
+    }
+
+    @RequestMapping(value = "/entrydetails/selected.csv", method = RequestMethod.GET) // TODO Unused
+    public void entrydetailsSelectedCSV(@RequestParam String members, HttpServletResponse response) throws Exception {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
+
+        List<Member> selectedMembers = Lists.newArrayList();
+        final Iterator<String> iterator = Splitter.on(",").split(members).iterator();
+        while (iterator.hasNext()) {
+            final String selectedMemberId = iterator.next();
+            log.info("Selected member id: " + selectedMemberId);
+            selectedMembers.add(loggedInUserApi.getMember(selectedMemberId));
+        }
+
+        csvOutputRenderer.renderCsvResponse(response, entryDetailsModelPopulator.getEntryDetailsHeaders(), entryDetailsModelPopulator.getEntryDetailsRows(selectedMembers));
+    }
 
 }
--- src/main/java/uk/co/squadlist/web/controllers/SquadsController.java
+++ src/main/java/uk/co/squadlist/web/controllers/SquadsController.java
@@ -1,14 +1,8 @@
 package uk.co.squadlist.web.controllers;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-
-import javax.validation.Valid;
-
+import com.google.common.base.Splitter;
+import com.google.common.collect.Sets;
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -18,15 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-
-import uk.co.eelpieconsulting.common.http.HttpBadRequestException;
 import uk.co.eelpieconsulting.common.http.HttpFetchException;
-import uk.co.eelpieconsulting.common.http.HttpForbiddenException;
-import uk.co.eelpieconsulting.common.http.HttpNotFoundException;
 import uk.co.squadlist.web.annotations.RequiresPermission;
 import uk.co.squadlist.web.api.InstanceSpecificApiClient;
 import uk.co.squadlist.web.api.SquadlistApi;
-import uk.co.squadlist.web.api.SquadlistApiFactory;
+import uk.co.squadlist.web.auth.LoggedInUserService;
 import uk.co.squadlist.web.context.InstanceConfig;
 import uk.co.squadlist.web.exceptions.InvalidSquadException;
 import uk.co.squadlist.web.exceptions.SignedInMemberRequiredException;
@@ -40,119 +30,126 @@ import uk.co.squadlist.web.services.Permission;
 import uk.co.squadlist.web.urls.UrlBuilder;
 import uk.co.squadlist.web.views.ViewFactory;
 
-import com.google.common.base.Splitter;
-import com.google.common.collect.Sets;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class SquadsController {
 
-	private final static Logger log = Logger.getLogger(SquadsController.class);
+    private final static Logger log = Logger.getLogger(SquadsController.class);
 
-	private final static Splitter COMMA_SPLITTER = Splitter.on(",");
+    private final static Splitter COMMA_SPLITTER = Splitter.on(",");
 
-	private InstanceSpecificApiClient instanceSpecificApiClient;
-	private UrlBuilder urlBuilder;
-	private ViewFactory viewFactory;
-	private SquadlistApi squadlistApi;
-	private InstanceConfig instanceConfig;
+    private final InstanceSpecificApiClient instanceSpecificApiClient;
+    private final UrlBuilder urlBuilder;
+    private final ViewFactory viewFactory;
+    private final InstanceConfig instanceConfig;
+    private final LoggedInUserService loggedInUserService;
 
-	public SquadsController() {
-	}
+    @Autowired
+    public SquadsController(InstanceSpecificApiClient instanceSpecificApiClient,
+                            UrlBuilder urlBuilder, ViewFactory viewFactory,
+                            InstanceConfig instanceConfig, LoggedInUserService loggedInUserService) {
+        this.instanceSpecificApiClient = instanceSpecificApiClient;
+        this.urlBuilder = urlBuilder;
+        this.viewFactory = viewFactory;
+        this.instanceConfig = instanceConfig;
+        this.loggedInUserService = loggedInUserService;
+    }
 
-	@Autowired
-	public SquadsController(InstanceSpecificApiClient instanceSpecificApiClient,
-							UrlBuilder urlBuilder, ViewFactory viewFactory, SquadlistApiFactory squadlistApiFactory,
-							InstanceConfig instanceConfig) throws IOException {
-		this.instanceSpecificApiClient = instanceSpecificApiClient;
-		this.urlBuilder = urlBuilder;
-		this.viewFactory = viewFactory;
-		this.squadlistApi = squadlistApiFactory.createClient();
-		this.instanceConfig = instanceConfig;
-	}
-
-	@RequestMapping(value="/squad/new", method=RequestMethod.GET)
+    @RequestMapping(value = "/squad/new", method = RequestMethod.GET)
     public ModelAndView newSquad(@ModelAttribute("squadDetails") SquadDetails squadDetails) throws Exception {
-		return renderNewSquadForm(new SquadDetails());
+        return renderNewSquadForm(new SquadDetails());
     }
 
-	@RequestMapping(value="/squad/new", method=RequestMethod.POST)
+    @RequestMapping(value = "/squad/new", method = RequestMethod.POST)
     public ModelAndView newSquadSubmit(@Valid @ModelAttribute("squadDetails") SquadDetails squadDetails, BindingResult result) throws UnknownInstanceException, SignedInMemberRequiredException {
-		if (result.hasErrors()) {
-			return renderNewSquadForm(squadDetails);
-		}
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
 
-		try {
-			Instance instance = squadlistApi.getInstance(instanceConfig.getInstance());
-			squadlistApi.createSquad(instance, squadDetails.getName());
+        if (result.hasErrors()) {
+            return renderNewSquadForm(squadDetails);
+        }
 
-			return new ModelAndView(new RedirectView(urlBuilder.adminUrl()));
+        try {
+            Instance instance = loggedInUserApi.getInstance(instanceConfig.getInstance());
+            loggedInUserApi.createSquad(instance, squadDetails.getName());
 
-		} catch (InvalidSquadException e) {
-			log.info("Invalid squad");
-			result.rejectValue("name", null, "squad name is already in use");
-			return renderNewSquadForm(squadDetails);
-		}
+            return new ModelAndView(new RedirectView(urlBuilder.adminUrl()));
+
+        } catch (InvalidSquadException e) {
+            log.info("Invalid squad");
+            result.rejectValue("name", null, "squad name is already in use");
+            return renderNewSquadForm(squadDetails);
+        }
     }
 
-	@RequiresPermission(permission=Permission.VIEW_ADMIN_SCREEN)
-	@RequestMapping(value="/squad/{id}/delete", method=RequestMethod.GET)
-	public ModelAndView deletePrompt(@PathVariable String id) throws UnknownSquadException, SignedInMemberRequiredException {
-		final Squad squad = squadlistApi.getSquad(id);
-		return viewFactory.getViewForLoggedInUser("deleteSquadPrompt").addObject("squad", squad).addObject("title", "Delete squad");
-	}
+    @RequiresPermission(permission = Permission.VIEW_ADMIN_SCREEN)
+    @RequestMapping(value = "/squad/{id}/delete", method = RequestMethod.GET)
+    public ModelAndView deletePrompt(@PathVariable String id) throws UnknownSquadException, SignedInMemberRequiredException {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
 
-	@RequiresPermission(permission=Permission.VIEW_ADMIN_SCREEN)
-	@RequestMapping(value="/squad/{id}/delete", method=RequestMethod.POST)
-	public ModelAndView delete(@PathVariable String id) throws UnknownSquadException {
-		final Squad squad = squadlistApi.getSquad(id);
-		squadlistApi.deleteSquad(squad);
-		return new ModelAndView(new RedirectView(urlBuilder.adminUrl()));
-	}
-
-	@RequestMapping(value="/squad/{id}/edit", method=RequestMethod.GET)
-	public ModelAndView editSquad(@PathVariable String id) throws UnknownSquadException, SignedInMemberRequiredException {
-		final Squad squad = squadlistApi.getSquad(id);
-
-		final SquadDetails squadDetails = new SquadDetails();
-		squadDetails.setName(squad.getName());
-
-		return renderEditSquadForm(squad, squadDetails);
-	}
-
-	@RequestMapping(value="/squad/{id}/edit", method=RequestMethod.POST)
-    public ModelAndView editSquadSubmit(@PathVariable String id, @Valid @ModelAttribute("squadDetails") SquadDetails squadDetails, BindingResult result) throws UnknownSquadException, JsonGenerationException, JsonMappingException, HttpNotFoundException, HttpBadRequestException, HttpForbiddenException, IOException, HttpFetchException, SignedInMemberRequiredException {
-		final Squad squad = squadlistApi.getSquad(id);
-		if (result.hasErrors()) {
-			return renderEditSquadForm(squad, squadDetails);
-		}
-
-		squad.setName(squadDetails.getName());
-		log.info("Updating squad: " + squad);
-		squadlistApi.updateSquad(squad);
-
-		final Set<String> updatedSquadMembers = Sets.newHashSet(COMMA_SPLITTER.split(squadDetails.getMembers()).iterator());
-		log.info("Setting squad members to " + updatedSquadMembers.size() + " members: " + updatedSquadMembers);
-		squadlistApi.setSquadMembers(squad.getId(), updatedSquadMembers);
-
-		return new ModelAndView(new RedirectView(urlBuilder.adminUrl()));
+        final Squad squad = loggedInUserApi.getSquad(id);
+        return viewFactory.getViewForLoggedInUser("deleteSquadPrompt").addObject("squad", squad).addObject("title", "Delete squad");
     }
 
-	private ModelAndView renderNewSquadForm(SquadDetails squadDetails) throws SignedInMemberRequiredException {
-		return viewFactory.getViewForLoggedInUser("newSquad").addObject("squadDetails", squadDetails);
-	}
+    @RequiresPermission(permission = Permission.VIEW_ADMIN_SCREEN)
+    @RequestMapping(value = "/squad/{id}/delete", method = RequestMethod.POST)
+    public ModelAndView delete(@PathVariable String id) throws UnknownSquadException, SignedInMemberRequiredException {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
 
-	private ModelAndView renderEditSquadForm(final Squad squad, final SquadDetails squadDetails) throws SignedInMemberRequiredException {
-		final ModelAndView mv = viewFactory.getViewForLoggedInUser("editSquad");
-		mv.addObject("squad", squad);
-		mv.addObject("squadDetails", squadDetails);
+        final Squad squad = loggedInUserApi.getSquad(id);
+        loggedInUserApi.deleteSquad(squad);
+        return new ModelAndView(new RedirectView(urlBuilder.adminUrl()));
+    }
 
-		final List<Member> squadMembers = squadlistApi.getSquadMembers(squad.getId());
-		mv.addObject("squadMembers", squadMembers);
+    @RequestMapping(value = "/squad/{id}/edit", method = RequestMethod.GET)
+    public ModelAndView editSquad(@PathVariable String id) throws UnknownSquadException, SignedInMemberRequiredException {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
 
-		final List<Member> availableMembers = instanceSpecificApiClient.getMembers();
-		availableMembers.removeAll(squadMembers);
-		mv.addObject("availableMembers", availableMembers);
-		return mv;
-	}
+        final Squad squad = loggedInUserApi.getSquad(id);
+
+        final SquadDetails squadDetails = new SquadDetails();
+        squadDetails.setName(squad.getName());
+
+        return renderEditSquadForm(squad, squadDetails, loggedInUserApi);
+    }
+
+    @RequestMapping(value = "/squad/{id}/edit", method = RequestMethod.POST)
+    public ModelAndView editSquadSubmit(@PathVariable String id, @Valid @ModelAttribute("squadDetails") SquadDetails squadDetails, BindingResult result) throws UnknownSquadException, IOException, HttpFetchException, SignedInMemberRequiredException {
+        SquadlistApi loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
+
+        final Squad squad = loggedInUserApi.getSquad(id);
+        if (result.hasErrors()) {
+            return renderEditSquadForm(squad, squadDetails, loggedInUserApi);
+        }
+
+        squad.setName(squadDetails.getName());
+        log.info("Updating squad: " + squad);
+        loggedInUserApi.updateSquad(squad);
+
+        final Set<String> updatedSquadMembers = Sets.newHashSet(COMMA_SPLITTER.split(squadDetails.getMembers()).iterator());
+        log.info("Setting squad members to " + updatedSquadMembers.size() + " members: " + updatedSquadMembers);
+        loggedInUserApi.setSquadMembers(squad.getId(), updatedSquadMembers);
+
+        return new ModelAndView(new RedirectView(urlBuilder.adminUrl()));
+    }
+
+    private ModelAndView renderNewSquadForm(SquadDetails squadDetails) throws SignedInMemberRequiredException {
+        return viewFactory.getViewForLoggedInUser("newSquad").addObject("squadDetails", squadDetails);
+    }
+
+    private ModelAndView renderEditSquadForm(final Squad squad, final SquadDetails squadDetails, SquadlistApi squadlistApi) throws SignedInMemberRequiredException {
+        final List<Member> squadMembers = squadlistApi.getSquadMembers(squad.getId());
+        final List<Member> availableMembers = instanceSpecificApiClient.getMembers();
+        availableMembers.removeAll(squadMembers);
+
+        return viewFactory.getViewForLoggedInUser("editSquad").
+                addObject("squad", squad).
+				addObject("squadDetails", squadDetails).
+				addObject("squadMembers", squadMembers).
+				addObject("availableMembers", availableMembers);
+    }
 
 }
