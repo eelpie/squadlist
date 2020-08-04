9fa7054f720e7ea70c45ba6ebefcaaba92110335
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-04 12:44:23 +0100

    Logging pings; helps with google cloud setup.

--- src/main/java/uk/co/squadlist/web/controllers/PingController.java
+++ src/main/java/uk/co/squadlist/web/controllers/PingController.java
@@ -1,5 +1,7 @@
 package uk.co.squadlist.web.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PingController {
 
+    private final static Logger log = LogManager.getLogger(PingController.class);
+
     @RequestMapping("/healthz")
     public ResponseEntity<String> ping() {
-        return new ResponseEntity<>(
-                "ok",
-                HttpStatus.OK);
+        log.info("Ping returning ok");
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
 }
