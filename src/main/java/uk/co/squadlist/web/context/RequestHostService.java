9ff78928433ae86cb91cdb78244f7834103f67e7
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-04 13:24:00 +0100

    Clean up; slightly more functional

--- src/main/java/uk/co/squadlist/web/context/RequestHostService.java
+++ src/main/java/uk/co/squadlist/web/context/RequestHostService.java
@@ -1,21 +1,20 @@
 package uk.co.squadlist.web.context;
 
-import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Component
 public class RequestHostService {
 
 	private final static Logger log = LogManager.getLogger(RequestHostService.class);
-
-	private static final String X_FORWARDED_HOST = "x-forwarded-host";
 
 	private final HttpServletRequest request;
 
@@ -25,21 +24,15 @@ public class RequestHostService {
 	}
 
 	public String getRequestHost() {
-		Enumeration<String> headerNames = request.getHeaderNames();
-		Joiner on = Joiner.on(", ");
-		log.info("All request headers: " + on.join(headerNames.asIterator()));
+		// The requested host name is likely to be on the host of forwarded host header
+		Stream<String> vhostHeaders = Lists.newArrayList("host", "x-forwarded-host").stream();
 
-		final String xForwardedHost = request.getHeader(X_FORWARDED_HOST);
-		if (!Strings.isNullOrEmpty(xForwardedHost)) {
-			log.debug("Request x-forwarded-host is: " + xForwardedHost);
-			return xForwardedHost;
+		Optional<String> firstHost = vhostHeaders.map(h -> request.getHeader(h)).filter(v -> !Strings.isNullOrEmpty(v)).findFirst();
+		if (firstHost.isPresent()) {
+			log.debug("Request host is: " + firstHost.get());
+			return firstHost.get();
 		}
 
-		final String host = request.getHeader("host");
-		if (!Strings.isNullOrEmpty(host)) {
-			log.debug("Request host is: " + host);
-			return host;
-		}
 		return null;
 	}
 
