e975026c58fa56a7984cf4b4eca49e87a58b7e3b
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-04-21 11:34:14 +0100

    Formatter for date expressed as DateTime by the Swagger API client.

--- src/main/java/uk/co/squadlist/web/views/DateFormatter.java
+++ src/main/java/uk/co/squadlist/web/views/DateFormatter.java
@@ -13,46 +13,50 @@ import uk.co.squadlist.web.context.Context;
 
 @Component
 public class DateFormatter {
-	
-	private final Context context;
-	
-	@Autowired
-	public DateFormatter(Context context) {
-		this.context = context;
-	}
-	
-	public String timeSince(Date date) {
-		return getDateFormatter().timeSince(date);
-	}
-	
-	public String month(String month) {
-		final DateTime monthDateTime = ISODateTimeFormat.yearMonth().parseDateTime(month);
-		return monthDateTime.toString("MMM yyyy");
-	}
 
-	public String dayMonthTime(Date date) {
-		return new DateTime(date, DateTimeZone.forID(context.getTimeZone())).toString("EEE dd MMM kk:mm");
-	}
-	
-	public String dayMonthYear(Date date) {
-		return getDateFormatter().dayMonthYear(date);
-	}
+    private final Context context;
 
-	public String dayMonthYear(java.time.OffsetDateTime offsetDateTime) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM YYYY");
-		return formatter.format(offsetDateTime);
-	}
+    @Autowired
+    public DateFormatter(Context context) {
+        this.context = context;
+    }
 
-	public String dayMonthYearTime(Date date) {
-		return getDateFormatter().dayMonthYearTime(date);
-	}
+    public String timeSince(Date date) {
+        return getDateFormatter().timeSince(date);
+    }
 
-	public String fullMonthYear(Date date) {
-		return getDateFormatter().fullMonthYear(date);
-	}
-	
-	private uk.co.eelpieconsulting.common.dates.DateFormatter getDateFormatter() {
-		return new uk.co.eelpieconsulting.common.dates.DateFormatter(context.getTimeZone());
-	}
-	
+    public String month(String month) {
+        final DateTime monthDateTime = ISODateTimeFormat.yearMonth().parseDateTime(month);
+        return monthDateTime.toString("MMM yyyy");
+    }
+
+    public String dayMonthTime(Date date) {
+        return new DateTime(date, DateTimeZone.forID(context.getTimeZone())).toString("EEE dd MMM kk:mm");
+    }
+
+    public String dayMonthYear(Date date) {
+        return getDateFormatter().dayMonthYear(date);
+    }
+
+    public String dayMonthYear(DateTime dateTime) {
+        return getDateFormatter().dayMonthYear(dateTime.toDate());
+    }
+
+    public String dayMonthYear(java.time.OffsetDateTime offsetDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM YYYY");
+        return formatter.format(offsetDateTime);
+    }
+
+    public String dayMonthYearTime(Date date) {
+        return getDateFormatter().dayMonthYearTime(date);
+    }
+
+    public String fullMonthYear(Date date) {
+        return getDateFormatter().fullMonthYear(date);
+    }
+
+    private uk.co.eelpieconsulting.common.dates.DateFormatter getDateFormatter() {
+        return new uk.co.eelpieconsulting.common.dates.DateFormatter(context.getTimeZone());
+    }
+
 }
