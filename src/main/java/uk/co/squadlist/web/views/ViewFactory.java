1ed4686bbba538b27ac6ae69a0b3c7c30264409e
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-07-28 10:55:43 +0100

    Eliminates preferedSquad addition to all views; dropdown actually uses the $squad model item which is added by squad specific controllers.

--- src/main/java/uk/co/squadlist/web/views/ViewFactory.java
+++ src/main/java/uk/co/squadlist/web/views/ViewFactory.java
@@ -1,39 +1,17 @@
 package uk.co.squadlist.web.views;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-import uk.co.squadlist.web.api.InstanceSpecificApiClient;
-import uk.co.squadlist.web.auth.LoggedInUserService;
 import uk.co.squadlist.web.exceptions.SignedInMemberRequiredException;
 import uk.co.squadlist.web.exceptions.UnknownInstanceException;
 import uk.co.squadlist.web.model.Member;
-import uk.co.squadlist.web.services.PreferredSquadService;
 
 @Component
 public class ViewFactory {
 
-    private final LoggedInUserService loggedInUserService;
-    private final PreferredSquadService preferredSquadService;
-
-    @Autowired
-    public ViewFactory(LoggedInUserService loggedInUserService,
-                       PreferredSquadService preferredSquadService) {
-        this.loggedInUserService = loggedInUserService;
-        this.preferredSquadService = preferredSquadService;
-    }
-
     public ModelAndView getViewForLoggedInUser(String templateName, Member loggedInUser) throws SignedInMemberRequiredException, UnknownInstanceException {
-        InstanceSpecificApiClient loggedInUserApi = loggedInUserService.getApiClientForLoggedInUser();
-
-        final ModelAndView mv = new ModelAndView(templateName);
-        try {
-            mv.addObject("preferredSquad", preferredSquadService.resolvedPreferredSquad(loggedInUser, loggedInUserApi.getSquads()));    // TODO this is questionable; push to navitems?
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return mv;
+        return new ModelAndView(templateName);
     }
 
     public ModelAndView redirectionTo(String url) {
