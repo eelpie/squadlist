369cb07ec74655cc97ea3679bd4dcdd3d7338046
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-08-17 19:47:54 +0100

    Contacts page nudges towards a functional wrapper type pattern; page specific code doesn't concern itself about signed in users and instance resolution.

--- src/main/kotlin/uk/co/squadlist/web/controllers/ContactsController.kt
+++ src/main/kotlin/uk/co/squadlist/web/controllers/ContactsController.kt
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import uk.co.squadlist.client.swagger.api.DefaultApi
+import uk.co.squadlist.model.swagger.Instance
+import uk.co.squadlist.model.swagger.Member
 import uk.co.squadlist.web.auth.LoggedInUserService
 import uk.co.squadlist.web.context.InstanceConfig
 import uk.co.squadlist.web.services.PreferredSquadService
@@ -20,27 +23,35 @@ class ContactsController @Autowired constructor(private val preferredSquadServic
                                                 private val instanceConfig: InstanceConfig) {
     @RequestMapping("/contacts")
     fun contacts(): ModelAndView {
-        val swaggerApiClientForLoggedInUser = loggedInUserService.swaggerApiClientForLoggedInUser
-        val instance = swaggerApiClientForLoggedInUser.getInstance(instanceConfig.instance)
-        val squads = swaggerApiClientForLoggedInUser.getSquads(instance.id)
-        val loggedInMember = loggedInUserService.loggedInMember
-        val navItems = navItemsBuilder.navItemsFor(loggedInMember, "contacts", swaggerApiClientForLoggedInUser, instance, squads)
-        return viewFactory.getViewFor("contacts", instance).addObject("title", "Contacts").addObject("mavItems", navItems).addObject("squads", squads) // TODO leaves squad null on view
+        val renderContactsPage = { instance: Instance, loggedInMember: Member, swaggerApiClientForLoggedInUser: DefaultApi ->
+            val squads = swaggerApiClientForLoggedInUser.getSquads(instance.id)
+            val navItems = navItemsBuilder.navItemsFor(loggedInMember, "contacts", swaggerApiClientForLoggedInUser, instance, squads)
+            viewFactory.getViewFor("contacts", instance).addObject("title", "Contacts").addObject("mavItems", navItems).addObject("squads", squads) // TODO leaves squad null on view
+        }
+        return withSignedInMember(renderContactsPage);
     }
 
     @RequestMapping("/contacts/{squadId}")
     fun squadContacts(@PathVariable squadId: String?): ModelAndView {
-        val swaggerApiClientForLoggedInUser = loggedInUserService.swaggerApiClientForLoggedInUser
-        val loggedInMember = loggedInUserService.loggedInMember
-        val instance = swaggerApiClientForLoggedInUser.getInstance(instanceConfig.instance)
-        val squadToShow = preferredSquadService.resolveSquad(squadId, swaggerApiClientForLoggedInUser, instance)
-        val squads = swaggerApiClientForLoggedInUser.getSquads(instance.id)
-        val navItems = navItemsBuilder.navItemsFor(loggedInMember, "contacts", swaggerApiClientForLoggedInUser, instance, squads)
-        val mv = viewFactory.getViewFor("contacts", instance).addObject("title", "Contacts").addObject("navItems", navItems).addObject("squads", squads)
-        if (!squads.isEmpty()) {
-            contactsModelPopulator.populateModel(squadToShow, mv, swaggerApiClientForLoggedInUser, loggedInMember)
+        val renderSquadContactsPage = { instance: Instance, loggedInMember: Member, swaggerApiClientForLoggedInUser: DefaultApi ->
+            val squadToShow = preferredSquadService.resolveSquad(squadId, swaggerApiClientForLoggedInUser, instance)
+            val squads = swaggerApiClientForLoggedInUser.getSquads(instance.id)
+            val navItems = navItemsBuilder.navItemsFor(loggedInMember, "contacts", swaggerApiClientForLoggedInUser, instance, squads)
+            val mv = viewFactory.getViewFor("contacts", instance).addObject("title", "Contacts").addObject("navItems", navItems).addObject("squads", squads)
+            if (!squads.isEmpty()) {
+                contactsModelPopulator.populateModel(squadToShow, mv, swaggerApiClientForLoggedInUser, loggedInMember)
+            }
+            mv
         }
-        return mv
+        return withSignedInMember(renderSquadContactsPage);
+    }
+
+    // Given a func to render a page, resolve the logged in member and instance then execute that function
+    private fun withSignedInMember(page: (Instance, Member, DefaultApi) -> ModelAndView): ModelAndView {
+        val swaggerApiClientForLoggedInUser = loggedInUserService.swaggerApiClientForLoggedInUser
+        val instance = swaggerApiClientForLoggedInUser.getInstance(instanceConfig.instance)
+        val loggedInMember = loggedInUserService.loggedInMember
+        return page(instance, loggedInMember, swaggerApiClientForLoggedInUser)
     }
 
 }
