0f8e21cb408b237155fa81565eb7f750f3aa728d
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-08-19 12:29:58 +0100

    Clean up; resolve governing body by id.

---
+++ src/main/kotlin/uk/co/squadlist/web/context/GoverningBodyFactory.kt
@@ -0,0 +1,23 @@
+package uk.co.squadlist.web.context
+
+import org.springframework.stereotype.Component
+import uk.co.squadlist.model.swagger.Instance
+import uk.co.squadlist.web.localisation.BritishRowing
+import uk.co.squadlist.web.localisation.GoverningBody
+import uk.co.squadlist.web.localisation.RowingIreland
+
+@Component
+class GoverningBodyFactory {
+    fun getGoverningBody(instance: Instance): GoverningBody {
+        return governingBodyFor(instance.governingBody)
+    }
+
+    fun governingBodyFor(id: String): GoverningBody {
+        return when (id) {
+            "rowing-ireland" -> RowingIreland() // TODO these ids want to be on the governing bodies
+            "british-rowing" -> BritishRowing()
+            else -> throw NotImplementedError()
+        }
+    }
+
+}
