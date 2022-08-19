99a287d0c8fb7d26041ccc7ff4eae810bdc745bc
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-08-19 20:05:41 +0100

    Missing governing bodies on old instances have been backfilled.

--- src/main/kotlin/uk/co/squadlist/web/context/GoverningBodyFactory.kt
+++ src/main/kotlin/uk/co/squadlist/web/context/GoverningBodyFactory.kt
@@ -9,22 +9,16 @@ import uk.co.squadlist.web.localisation.RowingIreland
 @Component
 class GoverningBodyFactory {
     fun getGoverningBody(instance: Instance): GoverningBody {
-        val id: String? = instance.governingBody
-        return governingBodyFor(id)
+        return governingBodyFor(instance.governingBody)
     }
 
-    fun governingBodyFor(id: String?): GoverningBody {
-        if (id != null) {           // TODO Some instances have no governing body set!
-
-            return when (id) {
-                "rowing-ireland" -> RowingIreland() // TODO these ids want to be on the governing bodies
-                "british-rowing" -> BritishRowing()
-                else -> {
-                    throw NotImplementedError()
-                }
+    fun governingBodyFor(id: String): GoverningBody {
+        return when (id) {
+            "rowing-ireland" -> RowingIreland() // TODO these ids want to be on the governing bodies
+            "british-rowing" -> BritishRowing()
+            else -> {
+                throw NotImplementedError()
             }
-        } else {
-            return BritishRowing()
         }
     }
 
