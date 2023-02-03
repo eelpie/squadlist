a78afe7c0c8a87670734d2338d205a6c76a35325
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-02-03 21:11:02 +0000

    Clean up; remove Mock annotations.

--- src/test/java/uk/co/squadlist/web/context/InstanceConfigTest.java
+++ src/test/java/uk/co/squadlist/web/context/InstanceConfigTest.java
@@ -1,31 +1,19 @@
 package uk.co.squadlist.web.context;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import uk.co.squadlist.web.context.InstanceConfig;
-import uk.co.squadlist.web.context.RequestHostService;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class InstanceConfigTest {
 
-	@Mock
-	private RequestHostService requestHostService;
-
-	private InstanceConfig instanceConfig;
-
-	@Before
-	public void setup() {
-		MockitoAnnotations.initMocks(this);
-		instanceConfig = new InstanceConfig(requestHostService, null);
-	}
+	private final RequestHostService requestHostService = mock(RequestHostService.class);
 
 	@Test
 	public void liveMultiTenantedInstanceShouldBeInferredFromTheRequestHostname() {
+		InstanceConfig instanceConfig =  new InstanceConfig(requestHostService, null);
+
 		when(requestHostService.getRequestHost()).thenReturn("aninstance.squadlist.co.uk");
 
 		assertEquals("aninstance", instanceConfig.getInstance());
@@ -33,6 +21,8 @@ public class InstanceConfigTest {
 
 	@Test
 	public void shouldHandleHyphensInIds() {
+		InstanceConfig instanceConfig =  new InstanceConfig(requestHostService, null);
+
 		when(requestHostService.getRequestHost()).thenReturn("an-instance.squadlist.co.uk");
 
 		assertEquals("an-instance", instanceConfig.getInstance());
@@ -40,11 +30,11 @@ public class InstanceConfigTest {
 
 	@Test
 	public void inDevelopmentEnvironmentsWeCanHardCodeTheInstanceNameSoThatWeCanDeployOnNonLiveUrls() {
-		instanceConfig = new InstanceConfig(requestHostService, "manuallyconfiguredinstance");
+		InstanceConfig instanceConfigWithManualInstance = new InstanceConfig(requestHostService, "manuallyconfiguredinstance");
 
 		when(requestHostService.getRequestHost()).thenReturn("aninstance.squadlist.co.uk");
 
-		assertEquals("manuallyconfiguredinstance", instanceConfig.getInstance());
+		assertEquals("manuallyconfiguredinstance", instanceConfigWithManualInstance.getInstance());
 	}
 
 }
