c006e690df7f9ccfb53049f3d5ab517025124d38
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-02-27 21:29:00 +0000

    Update test.

--- src/test/java/uk/co/squadlist/web/localisation/BritishRowingTest.java
+++ src/test/java/uk/co/squadlist/web/localisation/BritishRowingTest.java
@@ -45,9 +45,9 @@ public class BritishRowingTest {
 
 	@Test
 	public void effectiveAgeForMastersIsTheAgeTheyWouldReachBy31DecOfTheCurrentYear() {
-		assertEquals(48, britishRowing.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0)));
-		assertEquals(48, britishRowing.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0)));
-		assertEquals(48, britishRowing.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0)));
+		assertEquals(49, britishRowing.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0)));
+		assertEquals(49, britishRowing.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0)));
+		assertEquals(49, britishRowing.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0)));
 	}
 
 	@Test
--- src/test/java/uk/co/squadlist/web/localisation/RowingIrelandTest.java
+++ src/test/java/uk/co/squadlist/web/localisation/RowingIrelandTest.java
@@ -67,9 +67,9 @@ public class RowingIrelandTest {
 
 	@Test
 	public void effectiveAgeForMastersIsTheAgeTheyWouldReachBy31DecOfTheCurrentYear() {
-		assertEquals(48, rowingIreland.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0)));
-		assertEquals(48, rowingIreland.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0)));
-		assertEquals(48, rowingIreland.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0)));
+		assertEquals(49, rowingIreland.getEffectiveAge(new DateTime(1975, 12, 13, 0, 0, 0, 0)));
+		assertEquals(49, rowingIreland.getEffectiveAge(new DateTime(1975, 1, 1, 0, 0, 0, 0)));
+		assertEquals(49, rowingIreland.getEffectiveAge(new DateTime(1975, 12, 31, 0, 0, 0, 0)));
 	}
 
 	@Test
